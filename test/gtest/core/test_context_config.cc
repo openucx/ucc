6debcec27242d735a534302218aa258f589d8f98
Valentin Petrov <valentinp@nvidia.com>
2021-02-25 20:20:23 +0200

    CORE: ctx configuration for NUM EPS and PPN

--- test/gtest/core/test_context_config.cc
+++ test/gtest/core/test_context_config.cc
@@ -73,3 +73,27 @@ UCC_TEST_F(test_context_config, modify)
 
     ucc_context_config_release(ctx_config);
 }
+
+UCC_TEST_F(test_context_config, modify_core)
+{
+    ucc_context_config_h ctx_config;
+    unsigned             flags;
+    std::string          output;
+    flags = UCC_CONFIG_PRINT_CONFIG;
+    EXPECT_EQ(UCC_OK, ucc_context_config_read(lib_h, NULL, &ctx_config));
+
+    /* modify known field to expected value */
+    EXPECT_EQ(UCC_OK, ucc_context_config_modify(ctx_config, NULL, "ESTIMATED_NUM_EPS", "12345"));
+    testing::internal::CaptureStdout();
+    ucc_context_config_print(ctx_config, stdout, "", (ucc_config_print_flags_t)flags);
+    output = testing::internal::GetCapturedStdout();
+    EXPECT_NE(std::string::npos, output.find("UCC_ESTIMATED_NUM_EPS=12345"));
+
+    /* modify uknown field */
+    testing::internal::CaptureStdout();
+    EXPECT_NE(UCC_OK,
+              ucc_context_config_modify(ctx_config, NULL, "_UNKNOWN_FIELD", "_unknown_value"));
+    output = testing::internal::GetCapturedStdout();
+    EXPECT_NE(std::string::npos, output.find("failed to modify"));
+    ucc_context_config_release(ctx_config);
+}
