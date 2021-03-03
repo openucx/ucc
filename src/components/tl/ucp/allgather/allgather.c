478d81ed0da2d9eb7bd5bd7356f310ac2ced21d2
Valentin Petrov <valentinp@nvidia.com>
2021-03-03 09:26:42 +0200

    TL/UCP: allgather: check for user dtype

--- src/components/tl/ucp/allgather/allgather.c
+++ src/components/tl/ucp/allgather/allgather.c
@@ -12,6 +12,12 @@ ucc_status_t ucc_tl_ucp_allgather_ring_progress(ucc_coll_task_t *task);
 
 ucc_status_t ucc_tl_ucp_allgather_init(ucc_tl_ucp_task_t *task)
 {
+    if ((task->args.src.info.datatype == UCC_DT_USERDEFINED) ||
+        (task->args.dst.info.datatype == UCC_DT_USERDEFINED)) {
+        tl_error(UCC_TL_TEAM_LIB(task->team),
+                 "user defined datatype is not supported");
+        return UCC_ERR_NOT_SUPPORTED;
+    }
     task->super.post     = ucc_tl_ucp_allgather_ring_start;
     task->super.progress = ucc_tl_ucp_allgather_ring_progress;
     return UCC_OK;
