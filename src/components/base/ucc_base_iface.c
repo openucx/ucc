8b2d23965d7fa9c01a363e181caaa271ffaac753
Valentin Petrov <valentinp@nvidia.com>
2021-04-01 16:08:54 +0300

    CORE: coll score param description

--- src/components/base/ucc_base_iface.c
+++ src/components/base/ucc_base_iface.c
@@ -10,7 +10,15 @@ ucc_config_field_t ucc_base_config_table[] = {
      "poll.",
      ucc_offsetof(ucc_base_config_t, log_component), UCC_CONFIG_TYPE_LOG_COMP},
 
-    {"COLL_SCORE", "", "description",
+    {"COLL_SCORE", "", "Collective score modifier for a CL/TL component\n"
+     "format: \";\"-separated list of score values with optional qualifiers:\n"
+     "        <coll_type_1,..,coll_type_n>:<mem_type_1,..,mem_type_n>:"
+     "<msg_range_1,..,msg_range_n>:score\n"
+     "        msg_range has the format: start-end, where start,end - integers"
+     " or \"inf\"\n"
+     "        score: positive integer, 0 or inf; \n"
+     "               0 - disables the CL/TL in the given range for a given coll\n"
+     "               inf - forces the CL/TL in the given range for a given coll",
      ucc_offsetof(ucc_base_config_t, score_str), UCC_CONFIG_TYPE_STRING},
 
     {NULL}};
