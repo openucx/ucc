f4d33470fad3fb8f82533834f61e55a769b6f91c
Valentin Petrov <valentinp@nvidia.com>
2021-11-23 22:00:42 +0200

    TL/SHM: fix mem corruption in ctx destroy
        pass proper lib object

--- src/components/tl/shm/tl_shm_context.c
+++ src/components/tl/shm/tl_shm_context.c
@@ -14,15 +14,15 @@ UCC_CLASS_INIT_FUNC(ucc_tl_shm_context_t,
 {
 	ucc_tl_shm_context_config_t *tl_shm_config =
 	    ucc_derived_of(config, ucc_tl_shm_context_config_t);
+
     UCC_CLASS_CALL_SUPER_INIT(ucc_tl_context_t, tl_shm_config->super.tl_lib,
 	                              params->context);
-
     return UCC_OK;
 }
 
 UCC_CLASS_CLEANUP_FUNC(ucc_tl_shm_context_t)
 {
-    tl_info(&self->super, "finalizing tl context: %p", self);
+    tl_info(self->super.super.lib, "finalizing tl context: %p", self);
 }
 
 UCC_CLASS_DEFINE(ucc_tl_shm_context_t, ucc_tl_context_t);
