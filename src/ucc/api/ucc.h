4eeaf71a384077896ac77f81f044a9027288a506
Sergey Lebedev <sergeyle@nvidia.com>
2022-01-12 09:49:32 +0300

    DOCS: fix docs build (#380)

--- src/ucc/api/ucc.h
+++ src/ucc/api/ucc.h
@@ -225,8 +225,8 @@ typedef enum {
     UCC_GENERIC_DT_OPS_FLAG_CONTIG             = UCC_BIT(0), /*!< If set, the created datatype
                                                                   represents a contiguous memory
                                                                   region with the size specified
-                                                                  in @ref contig_size field of
-                                                                  @ref ucc_generic_dt_ops */
+                                                                  in @ref ucc_generic_dt_ops.contig_size
+                                                                  field of @ref ucc_generic_dt_ops */
     UCC_GENERIC_DT_OPS_FLAG_REDUCE             = UCC_BIT(1), /*!< If set, the created datatype
                                                                   has user-defined reduction
                                                                   operation associated with it.
@@ -249,7 +249,7 @@ typedef enum {
  * v_j = src2 + count * dt_extent * stride * j.
  * The result is stored in dst, so that
  * dst[i] = src1[i] + v0[i] + v1[i] + ... +v_nvecttors[i],
- * for i \in [0:count), where "+" represents user-defined reduction of 2 elements
+ * for i in [0:count), where "+" represents user-defined reduction of 2 elements
  */
 
 typedef struct ucc_reduce_cb_params {
@@ -1194,10 +1194,10 @@ enum ucc_team_attr_field {
  */
 enum ucc_team_flags {
     UCC_TEAM_FLAG_COLL_WORK_BUFFER             = UCC_BIT(0) /*< If set, this indicates
-                                                                the user will provide 
-                                                                a scratchpad buffer for 
-                                                                use in one-sided 
-                                                                collectives. Otherwise, 
+                                                                the user will provide
+                                                                a scratchpad buffer for
+                                                                use in one-sided
+                                                                collectives. Otherwise,
                                                                 an internal buffer will
                                                                 used. */
 };
@@ -1737,9 +1737,9 @@ typedef enum {
                                                             to be global on all the processes
                                                             participating in the collective.*/
     UCC_COLL_ARGS_FLAG_MEM_MAPPED_BUFFERS   = UCC_BIT(7)  /*!< If set, both src
-                                                            and dst buffers 
-                                                            reside in a memory 
-                                                            mapped region. 
+                                                            and dst buffers
+                                                            reside in a memory
+                                                            mapped region.
                                                             Useful for one-sided
                                                             collectives. */
 } ucc_coll_args_flags_t;
@@ -1848,14 +1848,14 @@ typedef struct ucc_coll_args {
                                              collectives */
     ucc_error_type_t                error_type; /*!< Error type */
     ucc_coll_id_t                   tag; /*!< Used for ordering collectives */
-    void                           *global_work_buffer; /*!< User allocated scratchpad 
-                                                             buffer for one-sided 
-                                                             collectives. The buffer 
-                                                             provided should be at least 
-                                                             the size returned by @ref 
-                                                             ucc_context_get_attr with 
-                                                             the field mask - 
-                                                             UCC_CONTEXT_ATTR_FIELD_WORK_BUFFER_SIZE 
+    void                           *global_work_buffer; /*!< User allocated scratchpad
+                                                             buffer for one-sided
+                                                             collectives. The buffer
+                                                             provided should be at least
+                                                             the size returned by @ref
+                                                             ucc_context_get_attr with
+                                                             the field mask -
+                                                             UCC_CONTEXT_ATTR_FIELD_WORK_BUFFER_SIZE
                                                              set to 1. */
     ucc_coll_callback_t             cb;
     double                          timeout; /*!< Timeout in seconds */
