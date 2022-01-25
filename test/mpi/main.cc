a573519a18a98f2649409005bdcd280056158af5
Ferrol Aderholdt <ferrol.aderholdt@gmail.com>
2022-01-25 08:44:12 -0600

    TEST/MPI: adds onesided alltoall test (#375)
    * TEST: add alltoall performance tests
    
    * TEST: initialize work buffer
    
    * REVIEW: address comments
    
    TEST/MPI: correct number of onesided tests
    
    * REVIEW: address feedback
    
    * CODESTYLE: fix code style
    
    * REVIEW: Address feedback
    
    Co-authored-by: ferrol aderholdt <faderholdt@nvidia.com>
    Co-authored-by: Manjunath Gorentla Venkata <manjugv@users.noreply.github.com>
    Co-authored-by: valentin petrov <valentinp@nvidia.com>

--- test/mpi/main.cc
+++ test/mpi/main.cc
@@ -15,6 +15,7 @@ static std::vector<ucc_coll_type_t> colls = {UCC_COLL_TYPE_BARRIER,
                                              UCC_COLL_TYPE_ALLTOALL,
                                              UCC_COLL_TYPE_ALLTOALLV,
                                              UCC_COLL_TYPE_REDUCE_SCATTER};
+static std::vector<ucc_coll_type_t> onesided_colls = {UCC_COLL_TYPE_ALLTOALL};
 static std::vector<ucc_memory_type_t> mtypes = {UCC_MEMORY_TYPE_HOST};
 static std::vector<ucc_datatype_t> dtypes = {UCC_DT_INT32, UCC_DT_INT64,
                                              UCC_DT_FLOAT32, UCC_DT_FLOAT64};
@@ -34,6 +35,7 @@ static ucc_thread_mode_t                   thread_mode  = UCC_THREAD_SINGLE;
 static int                                 iterations   = 1;
 static int                                 show_help    = 0;
 static int                                 num_tests    = 1;
+static bool                                has_onesided = true;
 #ifdef HAVE_CUDA
 static test_set_cuda_device_t test_cuda_set_device = TEST_SET_DEV_NONE;
 #endif
@@ -72,6 +74,7 @@ void PrintHelp()
        "--displ_bits <d1,d2,..>:        list of displacements bits: 32,64   (alltoallv only)\n"
        "--set_device <value>:           0 - don't set, 1 - cuda_device = local_rank, 2 - cuda_device = local_rank % cuda_device_count\n"
        "--num_tests  <value>:           number of tests to run in parallel\n"
+       "--onesided   <value>:           0 - no onesided tests, 1 - onesided tests\n"
        "--help:              Show help\n";
 }
 
@@ -333,7 +336,7 @@ void PrintInfo()
 
 int ProcessArgs(int argc, char** argv)
 {
-    const char *const short_opts  = "c:t:m:d:o:M:I:N:r:s:C:D:i:Z:ThS:";
+    const char *const short_opts  = "c:t:m:d:o:M:I:N:r:s:C:D:i:Z:ThSO:";
     const option      long_opts[] = {
                                 {"colls", required_argument, nullptr, 'c'},
                                 {"teams", required_argument, nullptr, 't'},
@@ -353,6 +356,7 @@ int ProcessArgs(int argc, char** argv)
 #ifdef HAVE_CUDA
                                 {"set_device", required_argument, nullptr, 'S'},
 #endif
+                                {"onesided", required_argument, nullptr, 'O'},
                                 {"help", no_argument, nullptr, 'h'},
                                 {nullptr, no_argument, nullptr, 0}
     };
@@ -416,6 +420,9 @@ int ProcessArgs(int argc, char** argv)
             test_cuda_set_device = (test_set_cuda_device_t)std::stoi(optarg);
             break;
 #endif
+        case 'O':
+            has_onesided = std::stoi(optarg);
+            break;
         case 'h':
             show_help = 1;
             break;
@@ -465,7 +472,7 @@ int main(int argc, char *argv[])
 #ifdef HAVE_CUDA
     set_cuda_device(test_cuda_set_device);
 #endif
-    test = new UccTestMpi(argc, argv, thread_mode, 0);
+    test = new UccTestMpi(argc, argv, thread_mode, 0, has_onesided);
     for (auto &m : mtypes) {
         if (UCC_MEMORY_TYPE_HOST != m && UCC_OK != ucc_mc_available(m)) {
             std::cerr << "requested memory type " << ucc_memory_type_names[m]
@@ -475,6 +482,9 @@ int main(int argc, char *argv[])
         }
     }
     test->create_teams(teams);
+    if (has_onesided) {
+        test->create_teams(teams, true);
+    }
     test->set_iter(iterations);
     test->set_num_tests(num_tests);
     test->set_colls(colls);
@@ -493,6 +503,13 @@ int main(int argc, char *argv[])
     for (auto &inpl : inplace) {
         test->set_inplace(inpl);
         test->run_all();
+    }
+    if (has_onesided) {
+        test->set_colls(onesided_colls);
+        for (auto &inpl : inplace) {
+            test->set_inplace(inpl);
+            test->run_all(true);
+        }
     }
     std::cout << std::flush;
     MPI_Iallreduce(MPI_IN_PLACE, test->results.data(), test->results.size(),
