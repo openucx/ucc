13f8b8c6f8d90472a9552ad377b0aeb86b3cffae
Shimmy Balsam <sbalsam@nvidia.com>
2022-04-14 18:40:00 +0300

    TL/SHM: adding layout to perf values

--- src/components/tl/shm/perf/amd/rome.c
+++ src/components/tl/shm/perf/amd/rome.c
@@ -9,10 +9,10 @@
 void ucc_tl_shm_perf_params_amd_rome_128_bcast(ucc_coll_task_t *coll_task)
 {
     ucc_tl_shm_task_t *task = ucc_derived_of(coll_task, ucc_tl_shm_task_t);
+    ucc_tl_shm_team_t *team = TASK_TEAM(task);
     size_t             data_size = ucc_coll_args_msgsize(&task->super.bargs);
 
-    if (TASK_LIB(task)->cfg.set_perf_params) {
-        if (data_size < 256) {
+        if (data_size <= team->max_inline) {
             task->progress_alg   = BCAST_WW;
             task->base_tree_only = 1;
             task->base_radix     = 4;
@@ -23,27 +23,21 @@ void ucc_tl_shm_perf_params_amd_rome_128_bcast(ucc_coll_task_t *coll_task)
             task->base_radix     = 16;
             task->top_radix      = TASK_LIB(task)->cfg.bcast_top_radix;
         }
-    } else {
-        ucc_tl_shm_perf_params_generic_bcast(coll_task);
-    }
 }
 
 void ucc_tl_shm_perf_params_amd_rome_128_reduce(ucc_coll_task_t *coll_task)
 {
     ucc_tl_shm_task_t *task = ucc_derived_of(coll_task, ucc_tl_shm_task_t);
+    ucc_tl_shm_team_t *team = TASK_TEAM(task);
     size_t             data_size = ucc_coll_args_msgsize(&task->super.bargs);
 
-    if (TASK_LIB(task)->cfg.set_perf_params) {
-        if (data_size < 256) {
+        if (data_size <= team->max_inline) {
             task->base_tree_only = 0;
-            task->base_radix     = 4;
+            task->base_radix     = 2;
             task->top_radix      = TASK_LIB(task)->cfg.reduce_top_radix;
         } else {
             task->base_tree_only = 0;
             task->base_radix     = 2;
             task->top_radix      = TASK_LIB(task)->cfg.reduce_top_radix;
         }
-    } else {
-        ucc_tl_shm_perf_params_generic_reduce(coll_task);
-    }
 }
--- src/components/tl/shm/perf/intel/broadwell.c
+++ src/components/tl/shm/perf/intel/broadwell.c
@@ -9,32 +9,29 @@
 void ucc_tl_shm_perf_params_intel_broadwell_28_bcast(ucc_coll_task_t *coll_task)
 {
     ucc_tl_shm_task_t *task = ucc_derived_of(coll_task, ucc_tl_shm_task_t);
+    ucc_tl_shm_team_t *team = TASK_TEAM(task);
     size_t             data_size = ucc_coll_args_msgsize(&task->super.bargs);
 
-    if (TASK_LIB(task)->cfg.set_perf_params) {
-        if (data_size < 256) {
-            task->progress_alg   = BCAST_WW;
-            task->base_tree_only = 1;
-            task->base_radix     = 4;
-            task->top_radix      = 0;
-        } else {
+        if (data_size <= team->max_inline) {
             task->progress_alg   = BCAST_WR;
             task->base_tree_only = 0;
             task->base_radix     = 4;
             task->top_radix      = TASK_LIB(task)->cfg.bcast_top_radix;
+        } else {
+            task->progress_alg   = BCAST_WR;
+            task->base_tree_only = 0;
+            task->base_radix     = 14;
+            task->top_radix      = TASK_LIB(task)->cfg.bcast_top_radix;
         }
-    } else {
-        ucc_tl_shm_perf_params_generic_bcast(coll_task);
-    }
 }
 
 void ucc_tl_shm_perf_params_intel_broadwell_14_bcast(ucc_coll_task_t *coll_task)
 {
     ucc_tl_shm_task_t *task = ucc_derived_of(coll_task, ucc_tl_shm_task_t);
+    ucc_tl_shm_team_t *team = TASK_TEAM(task);
     size_t             data_size = ucc_coll_args_msgsize(&task->super.bargs);
 
-    if (TASK_LIB(task)->cfg.set_perf_params) {
-        if (data_size < 256) {
+        if (data_size <= team->max_inline) {
             task->progress_alg   = BCAST_WR;
             task->base_tree_only = 1;
             task->base_radix     = 7;
@@ -45,18 +42,15 @@ void ucc_tl_shm_perf_params_intel_broadwell_14_bcast(ucc_coll_task_t *coll_task)
             task->base_radix     = 8;
             task->top_radix      = 0;
         }
-    } else {
-        ucc_tl_shm_perf_params_generic_bcast(coll_task);
-    }
 }
 
 void ucc_tl_shm_perf_params_intel_broadwell_8_bcast(ucc_coll_task_t *coll_task)
 {
     ucc_tl_shm_task_t *task = ucc_derived_of(coll_task, ucc_tl_shm_task_t);
+    ucc_tl_shm_team_t *team = TASK_TEAM(task);
     size_t             data_size = ucc_coll_args_msgsize(&task->super.bargs);
 
-    if (TASK_LIB(task)->cfg.set_perf_params) {
-        if (data_size < 256) {
+        if (data_size <= team->max_inline) {
             task->progress_alg   = BCAST_WW;
             task->base_tree_only = 1;
             task->base_radix     = 7;
@@ -67,40 +61,34 @@ void ucc_tl_shm_perf_params_intel_broadwell_8_bcast(ucc_coll_task_t *coll_task)
             task->base_radix     = 7;
             task->top_radix      = 0;
         }
-    } else {
-        ucc_tl_shm_perf_params_generic_bcast(coll_task);
-    }
 }
 
 void ucc_tl_shm_perf_params_intel_broadwell_28_reduce(
     ucc_coll_task_t *coll_task)
 {
     ucc_tl_shm_task_t *task = ucc_derived_of(coll_task, ucc_tl_shm_task_t);
+    ucc_tl_shm_team_t *team = TASK_TEAM(task);
     size_t             data_size = ucc_coll_args_msgsize(&task->super.bargs);
 
-    if (TASK_LIB(task)->cfg.set_perf_params) {
-        if (data_size < 256) {
-            task->base_tree_only = 1;
-            task->base_radix     = 4;
-            task->top_radix      = 0;
+        if (data_size <= team->max_inline) {
+            task->base_tree_only = 0;
+            task->base_radix     = 8;
+            task->top_radix      = TASK_LIB(task)->cfg.reduce_top_radix;
         } else {
-            task->base_tree_only = 1;
+            task->base_tree_only = 0;
             task->base_radix     = 2;
-            task->top_radix      = 0;
+            task->top_radix      = TASK_LIB(task)->cfg.reduce_top_radix;
         }
-    } else {
-        ucc_tl_shm_perf_params_generic_reduce(coll_task);
-    }
 }
 
 void ucc_tl_shm_perf_params_intel_broadwell_14_reduce(
     ucc_coll_task_t *coll_task)
 {
     ucc_tl_shm_task_t *task = ucc_derived_of(coll_task, ucc_tl_shm_task_t);
+    ucc_tl_shm_team_t *team = TASK_TEAM(task);
     size_t             data_size = ucc_coll_args_msgsize(&task->super.bargs);
 
-    if (TASK_LIB(task)->cfg.set_perf_params) {
-        if (data_size < 256) {
+        if (data_size <= team->max_inline) {
             task->base_tree_only = 1;
             task->base_radix     = 4;
             task->top_radix      = 0;
@@ -109,18 +97,15 @@ void ucc_tl_shm_perf_params_intel_broadwell_14_reduce(
             task->base_radix     = 2;
             task->top_radix      = 0;
         }
-    } else {
-        ucc_tl_shm_perf_params_generic_reduce(coll_task);
-    }
 }
 
 void ucc_tl_shm_perf_params_intel_broadwell_8_reduce(ucc_coll_task_t *coll_task)
 {
     ucc_tl_shm_task_t *task = ucc_derived_of(coll_task, ucc_tl_shm_task_t);
+    ucc_tl_shm_team_t *team = TASK_TEAM(task);
     size_t             data_size = ucc_coll_args_msgsize(&task->super.bargs);
 
-    if (TASK_LIB(task)->cfg.set_perf_params) {
-        if (data_size < 256) {
+        if (data_size <= team->max_inline) {
             task->base_tree_only = 1;
             task->base_radix     = 7;
             task->top_radix      = 0;
@@ -129,7 +114,4 @@ void ucc_tl_shm_perf_params_intel_broadwell_8_reduce(ucc_coll_task_t *coll_task)
             task->base_radix     = 2;
             task->top_radix      = 0;
         }
-    } else {
-        ucc_tl_shm_perf_params_generic_reduce(coll_task);
-    }
 }
