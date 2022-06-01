919a4a5d0ebb8e457548e5b16cf01de4cb2fec20
Sergey Lebedev <sergeyle@nvidia.com>
2022-06-01 18:56:14 +0300

    TL/CUDA: multiring allgatherv and reduce_scatterv (#494)
    * TL/CUDA: multiring allgatherv
    
    * TL/CUDA: multiring reduce_scatterv
    
    * TL/CUDA: fix review comments

--- src/components/tl/cuda/allgather/allgather_ring.c
+++ src/components/tl/cuda/allgather/allgather_ring.c
@@ -23,11 +23,7 @@ size_t ucc_tl_cuda_allgather_get_offset(const ucc_tl_cuda_task_t *task,
 
 ucc_status_t ucc_tl_cuda_allgather_ring_init(ucc_tl_cuda_task_t *task)
 {
-    ucc_tl_cuda_team_t *team  = TASK_TEAM(task);
-    ucc_coll_args_t    *args  = &TASK_ARGS(task);
-    ucc_rank_t          tsize = UCC_TL_TEAM_SIZE(team);
-    size_t              ssize = UCC_TL_CUDA_TEAM_LIB(team)->cfg.scratch_size;
-    size_t              send_size, frag_size;
+    ucc_coll_args_t *args  = &TASK_ARGS(task);
 
     task->allgatherv_ring.get_count  = ucc_tl_cuda_allgather_get_count;
     task->allgatherv_ring.get_offset = ucc_tl_cuda_allgather_get_offset;
@@ -35,13 +31,7 @@ ucc_status_t ucc_tl_cuda_allgather_ring_init(ucc_tl_cuda_task_t *task)
     task->allgatherv_ring.sbuf       = args->src.info.buffer;
     task->allgatherv_ring.rbuf       = args->dst.info.buffer;
 
-
-    send_size = (args->dst.info.count / tsize) *
-                ucc_dt_size(args->dst.info.datatype);
-    frag_size = ucc_min(ssize/2, send_size);
-
     task->super.flags               |= UCC_COLL_TASK_FLAG_EXECUTOR;
-    task->allgatherv_ring.num_frags = ucc_div_round_up(send_size, frag_size);
     task->super.post                = ucc_tl_cuda_allgatherv_ring_start;
     task->super.triggered_post      = ucc_triggered_post;
     task->super.progress            = ucc_tl_cuda_allgatherv_ring_progress;
