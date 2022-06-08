efc3902484e05cb48fbd7331a84def95eec290bb
Ching-Hsiang Chu <chchu@fb.com>
2022-06-08 10:43:08 -0700

    CODESTYLE: address comments

--- src/components/tl/self/tl_self_lib.c
+++ src/components/tl/self/tl_self_lib.c
@@ -13,6 +13,7 @@ UCC_CLASS_INIT_FUNC(ucc_tl_self_lib_t, const ucc_base_lib_params_t *params,
 {
     const ucc_tl_self_lib_config_t *tl_config =
         ucc_derived_of(config, ucc_tl_self_lib_config_t);
+
     UCC_CLASS_CALL_SUPER_INIT(ucc_tl_lib_t, &ucc_tl_self.super,
                               &tl_config->super);
     memcpy(&self->cfg, tl_config, sizeof(*tl_config));
@@ -31,6 +32,7 @@ ucc_status_t ucc_tl_self_get_lib_attr(const ucc_base_lib_t *lib, /* NOLINT */
                                       ucc_base_lib_attr_t  *base_attr)
 {
     ucc_tl_lib_attr_t *attr      = ucc_derived_of(base_attr, ucc_tl_lib_attr_t);
+
     attr->super.attr.thread_mode = UCC_THREAD_MULTIPLE;
     attr->super.attr.coll_types  = UCC_TL_SELF_SUPPORTED_COLLS;
     return UCC_OK;
