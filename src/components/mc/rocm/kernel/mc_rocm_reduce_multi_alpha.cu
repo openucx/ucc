a98964c71b82227995bd439fd44e67ef1606b907
Edgar Gabriel <edgargabriel@users.noreply.github.com>
2022-06-30 15:08:22 -0500

    MC/ROCM: resync component adding complex datatypes (#552)
    This commits resync the rocm components with the ucc code base.
    Specifically, it adds support (or disqualifies itself) for various
    float(32,64,128)_complex datatypes.
    
    Co-authored-by: Sergey Lebedev <sergeyle@nvidia.com>

--- src/components/mc/rocm/kernel/mc_rocm_reduce_multi_alpha.cu
+++ src/components/mc/rocm/kernel/mc_rocm_reduce_multi_alpha.cu
@@ -45,6 +45,36 @@
         }                                                                      \
     }
 
+__global__ void UCC_REDUCE_ALPHA_ROCM_SUM_WITH_PROD_COMPLEX(
+    const hipFloatComplex *s1, const hipFloatComplex *s2, hipFloatComplex *d,
+    size_t size, size_t count, size_t ld, double alpha)
+{
+    size_t start = blockIdx.x * blockDim.x + threadIdx.x;
+    size_t step  = blockDim.x * gridDim.x;
+    for (size_t i = start; i < count; i += step) {
+        d[i] = DO_OP_SUM_FLOAT_COMPLEX(s1[i], s2[i]);
+        for (size_t j = 1; j < size; j++) {
+            d[i] = DO_OP_SUM_FLOAT_COMPLEX(d[i], s2[i + j * ld]);
+        }
+        d[i] = DO_OP_PROD_SCALAR_FLOAT_COMPLEX(d[i], alpha);
+    }
+}
+
+__global__ void UCC_REDUCE_ALPHA_ROCM_SUM_WITH_PROD_COMPLEX(
+    const hipDoubleComplex *s1, const hipDoubleComplex *s2, hipDoubleComplex *d,
+    size_t size, size_t count, size_t ld, double alpha)
+{
+    size_t start = blockIdx.x * blockDim.x + threadIdx.x;
+    size_t step  = blockDim.x * gridDim.x;
+    for (size_t i = start; i < count; i += step) {
+        d[i] = DO_OP_SUM_DOUBLE_COMPLEX(s1[i], s2[i]);
+        for (size_t j = 1; j < size; j++) {
+            d[i] = DO_OP_SUM_DOUBLE_COMPLEX(d[i], s2[i + j * ld]);
+        }
+        d[i] = DO_OP_PROD_SCALAR_DOUBLE_COMPLEX(d[i], alpha);
+    }
+}
+
 ROCM_REDUCE_ALPHA_WITH_OP(SUM_WITH_PROD, DO_OP_SUM, DO_OP_PROD)
 
 ROCM_REDUCE_ALPHA_WITH_OP_SPECIALIZED(SUM_WITH_PROD, DO_OP_SUM_HALF, __half,
@@ -59,6 +89,14 @@ ROCM_REDUCE_ALPHA_WITH_OP_SPECIALIZED(SUM_WITH_PROD, DO_OP_SUM_BFLOAT16,
         UCC_REDUCE_ALPHA_ROCM_##NAME<type>                                     \
             <<<b, t, 0, s>>>(src1, src2, dest, size, count, ld, alpha);        \
     } while (0)
+
+#define LAUNCH_KERNEL_COMPLEX(NAME, type, src1, src2, dest, size, count, ld,   \
+                              alpha, s, b, t)                                  \
+    do {                                                                       \
+        UCC_REDUCE_ALPHA_ROCM_SUM_WITH_PROD_COMPLEX<<<b, t, 0, s>>>(           \
+            src1, src2, dest, size, count, ld, alpha);                         \
+    } while (0)
+
 
 #define DT_REDUCE_FLOAT(type, reduce_op, src1_p, src2_p, dest_p, size, count,  \
                         ld, alpha, vector_op, s, b, t)                         \
@@ -84,6 +122,36 @@ ROCM_REDUCE_ALPHA_WITH_OP_SPECIALIZED(SUM_WITH_PROD, DO_OP_SUM_BFLOAT16,
         default:                                                               \
             mc_error(&ucc_mc_rocm.super,                                       \
                      "float dtype does not support "                           \
+                     "requested vector op: %s",                                \
+                     ucc_reduction_op_str(vector_op));                         \
+            return UCC_ERR_NOT_SUPPORTED;                                      \
+        }                                                                      \
+    } while (0)
+
+#define DT_REDUCE_FLOAT_COMPLEX(type, reduce_op, src1_p, src2_p, dest_p, size, \
+                                count, ld, alpha, vector_op, s, b, t)          \
+    do {                                                                       \
+        const type *sbuf1 = (const type *)src1_p;                              \
+        const type *sbuf2 = (const type *)src2_p;                              \
+        type *      dest  = (type *)dest_p;                                    \
+        switch (vector_op) {                                                   \
+        case UCC_OP_PROD:                                                      \
+            switch (reduce_op) {                                               \
+            case UCC_OP_AVG:                                                   \
+                UCC_REDUCE_ALPHA_ROCM_SUM_WITH_PROD_COMPLEX<<<b, t, 0, s>>>(   \
+                    sbuf1, sbuf2, dest, size, count, ld, alpha);               \
+                break;                                                         \
+            default:                                                           \
+                mc_error(&ucc_mc_rocm.super,                                   \
+                         "float complex dtype does not support "               \
+                         "requested reduce op: %s",                            \
+                         ucc_reduction_op_str(reduce_op));                     \
+                return UCC_ERR_NOT_SUPPORTED;                                  \
+            }                                                                  \
+            break;                                                             \
+        default:                                                               \
+            mc_error(&ucc_mc_rocm.super,                                       \
+                     "float complex dtype does not support "                   \
                      "requested vector op: %s",                                \
                      ucc_reduction_op_str(vector_op));                         \
             return UCC_ERR_NOT_SUPPORTED;                                      \
@@ -125,6 +193,14 @@ ucc_mc_rocm_reduce_multi_alpha(const void *src1, const void *src2, void *dst,
         ucc_assert(8 == sizeof(double));
         DT_REDUCE_FLOAT(double, reduce_op, src1, src2, dst, n_vectors, count,
                         ld, alpha, vector_op, stream, bk, th);
+        break;
+    case UCC_DT_FLOAT32_COMPLEX:
+        DT_REDUCE_FLOAT_COMPLEX(hipFloatComplex, reduce_op, src1, src2, dst, n_vectors,
+                                count, ld, alpha, vector_op, stream, bk, th);
+        break;
+    case UCC_DT_FLOAT64_COMPLEX:
+        DT_REDUCE_FLOAT_COMPLEX(hipDoubleComplex, reduce_op, src1, src2, dst, n_vectors,
+                                count, ld, alpha, vector_op, stream, bk, th);
         break;
     case UCC_DT_BFLOAT16:
         DT_REDUCE_FLOAT(hip_bfloat16, reduce_op, src1, src2, dst, n_vectors,
