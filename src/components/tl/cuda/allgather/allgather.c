786275c84ee03d801598dda832966876027c1966
Sergey Lebedev <sergeyle@nvidia.com>
2022-07-19 23:37:05 +0300

    TL/CUDA: allgather(v) linear alg (#561)
    * TL/CUDA: allgather(v) linear alg
    
    * REVIEW: fix review comments
    
    * addeed algorithm description
    * fixed alignment
    * fixed copyright
    
    * REVIEW: apply clang format
    
    Co-authored-by: valentin petrov <valentinp@nvidia.com>

--- src/components/tl/cuda/allgather/allgather.c
+++ src/components/tl/cuda/allgather/allgather.c
@@ -7,29 +7,40 @@
 #include "allgather.h"
 #include "components/mc/ucc_mc.h"
 
-ucc_status_t ucc_tl_cuda_allgather_ring_init(ucc_tl_cuda_task_t *task);
+ucc_base_coll_alg_info_t
+    ucc_tl_cuda_allgather_algs[UCC_TL_CUDA_ALLGATHER_ALG_LAST + 1] = {
+        [UCC_TL_CUDA_ALLGATHER_ALG_AUTO] =
+            {.id   = UCC_TL_CUDA_ALLGATHER_ALG_AUTO,
+             .name = "auto",
+             .desc = "choose allgather algorithm based on CUDA topology"},
+        [UCC_TL_CUDA_ALLGATHER_ALG_RING] =
+            {.id   = UCC_TL_CUDA_ALLGATHER_ALG_RING,
+             .name = "ring",
+             .desc = "multiring allgather algorithm"},
+        [UCC_TL_CUDA_ALLGATHER_ALG_LINEAR] =
+            {.id   = UCC_TL_CUDA_ALLGATHER_ALG_LINEAR,
+             .name = "linear",
+             .desc = "linear allgather algorithm"},
+        [UCC_TL_CUDA_ALLGATHER_ALG_LAST] = {
+            .id = 0, .name = NULL, .desc = NULL}};
+
+size_t ucc_tl_cuda_allgather_get_count(const ucc_tl_cuda_task_t *task,
+                                       ucc_rank_t                block)
+{
+    return TASK_ARGS(task).dst.info.count / UCC_TL_TEAM_SIZE(TASK_TEAM(task));
+}
+
+size_t ucc_tl_cuda_allgather_get_offset(const ucc_tl_cuda_task_t *task,
+                                        ucc_rank_t                block)
+{
+    return (TASK_ARGS(task).dst.info.count /
+            UCC_TL_TEAM_SIZE(TASK_TEAM(task))) *
+           block;
+}
 
 ucc_status_t ucc_tl_cuda_allgather_init(ucc_base_coll_args_t *coll_args,
                                         ucc_base_team_t *tl_team,
                                         ucc_coll_task_t **task_p)
 {
-    ucc_tl_cuda_team_t *team = ucc_derived_of(tl_team, ucc_tl_cuda_team_t);
-    ucc_tl_cuda_task_t *task = ucc_tl_cuda_task_init(coll_args, team);
-    ucc_status_t status;
-
-    if (ucc_unlikely(!task)) {
-        return UCC_ERR_NO_MEMORY;
-    }
-
-    status = ucc_tl_cuda_allgather_ring_init(task);
-    if (ucc_unlikely(status != UCC_OK)) {
-        goto free_task;
-    }
-
-    *task_p = &task->super;
-    return UCC_OK;
-
-free_task:
-    ucc_tl_cuda_task_put(task);
-    return status;
+    return ucc_tl_cuda_allgather_ring_init(coll_args, tl_team, task_p);
 }
--- src/components/tl/cuda/allgather/allgather.h
+++ src/components/tl/cuda/allgather/allgather.h
@@ -10,8 +10,46 @@
 #include "tl_cuda.h"
 #include "tl_cuda_coll.h"
 
+enum
+{
+    UCC_TL_CUDA_ALLGATHER_ALG_AUTO,
+    UCC_TL_CUDA_ALLGATHER_ALG_RING,
+    UCC_TL_CUDA_ALLGATHER_ALG_LINEAR,
+    UCC_TL_CUDA_ALLGATHER_ALG_LAST
+};
+
+extern ucc_base_coll_alg_info_t
+    ucc_tl_cuda_allgather_algs[UCC_TL_CUDA_ALLGATHER_ALG_LAST + 1];
+
+#define UCC_TL_CUDA_ALLGATHER_DEFAULT_ALG_SELECT_STR "allgather:cuda:@0"
+
+size_t ucc_tl_cuda_allgather_get_count(const ucc_tl_cuda_task_t *task,
+                                       ucc_rank_t                block);
+
+size_t ucc_tl_cuda_allgather_get_offset(const ucc_tl_cuda_task_t *task,
+                                        ucc_rank_t                block);
+
 ucc_status_t ucc_tl_cuda_allgather_init(ucc_base_coll_args_t *coll_args,
                                         ucc_base_team_t *tl_team,
                                         ucc_coll_task_t **task_p);
+
+ucc_status_t ucc_tl_cuda_allgather_ring_init(ucc_base_coll_args_t *coll_args,
+                                             ucc_base_team_t *     tl_team,
+                                             ucc_coll_task_t **    task_p);
+
+ucc_status_t ucc_tl_cuda_allgather_linear_init(ucc_base_coll_args_t *coll_args,
+                                               ucc_base_team_t *     tl_team,
+                                               ucc_coll_task_t **    task_p);
+
+static inline int ucc_tl_cuda_allgather_alg_from_str(const char *str)
+{
+    int i;
+    for (i = 0; i < UCC_TL_CUDA_ALLGATHER_ALG_LAST; i++) {
+        if (0 == strcasecmp(str, ucc_tl_cuda_allgather_algs[i].name)) {
+            break;
+        }
+    }
+    return i;
+}
 
 #endif
