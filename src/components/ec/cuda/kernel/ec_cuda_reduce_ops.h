a654f295a50ec9bbeeee6718357b0c3d1bf47f36
snordmann <snordmann@nvidia.com>
2022-11-24 12:35:53 +0200

    CODESTYLE: clang-tidy cleanup

--- src/components/ec/cuda/kernel/ec_cuda_reduce_ops.h
+++ src/components/ec/cuda/kernel/ec_cuda_reduce_ops.h
@@ -59,87 +59,87 @@ cuFloatComplex operator* (const cuFloatComplex & first,
                                cuCimagf(first) * second);
 }
 
-#define CUDA_REDUCE_WITH_OP_DEFAULT(NAME, _OP)                                  \
-    template <typename _Type, typename _AlphaType, bool triggered, int UNROLL>  \
-    __device__ ucc_status_t ucc_reduce_cuda_default_##NAME(                     \
-        ucc_eee_task_reduce_t task, uint16_t flags)                             \
-    {                                                                           \
-        const size_t count  = task.count;                                       \
-        _Type *      d      = (_Type *)task.dst;                                \
-        const uint16_t    n_srcs = task.n_srcs;                                      \
-        const int    warp =                                                     \
+#define CUDA_REDUCE_WITH_OP_DEFAULT(NAME, _OP)                                    \
+    template <typename _Type, typename _AlphaType, bool triggered, int UNROLL>    \
+    __device__ ucc_status_t ucc_reduce_cuda_default_##NAME(                       \
+        ucc_eee_task_reduce_t task, uint16_t flags)                               \
+    {                                                                             \
+        const size_t   count  = task.count;                                       \
+        _Type *        d      = (_Type *)task.dst;                                \
+        const uint16_t n_srcs = task.n_srcs;                                      \
+        const int      warp =                                                     \
             triggered ? threadIdx.x / WARP_SIZE                              \
-                         : (threadIdx.x + blockIdx.x * blockDim.x) / WARP_SIZE; \
-        const int    num_warps = triggered                                      \
-                                     ? blockDim.x / WARP_SIZE                   \
-                                     : (blockDim.x * gridDim.x) / WARP_SIZE;    \
-        const int    idx       = threadIdx.x % WARP_SIZE;                       \
-        const size_t num_lines =                                                \
-            (count / (WARP_SIZE * UNROLL)) * (WARP_SIZE * UNROLL);              \
-        const _Type *s[UCC_EE_EXECUTOR_NUM_BUFS];                               \
-        _Type        tmp1[UNROLL];                                              \
-        _Type        tmp2[UNROLL];                                              \
-        size_t       i, j;                                                      \
-        memcpy(s, task.srcs, UCC_EE_EXECUTOR_NUM_BUFS * sizeof(_Type *));       \
-        for (size_t line = warp * WARP_SIZE * UNROLL + idx; line < num_lines;   \
-             line += num_warps * WARP_SIZE * UNROLL) {                          \
-            _Pragma("unroll") for (i = 0; i < UNROLL; i++)                      \
-            {                                                                   \
-                tmp1[i] = s[0][line + WARP_SIZE * i];                           \
-            }                                                                   \
-            for (j = 1; j < UCC_EE_EXECUTOR_NUM_BUFS; j++) {                    \
-                if (j >= n_srcs) {                                              \
-                    break;                                                      \
-                }                                                               \
-                _Pragma("unroll") for (i = 0; i < UNROLL; i++)                  \
-                {                                                               \
-                    tmp2[i] = s[j][line + WARP_SIZE * i];                       \
-                }                                                               \
-                _Pragma("unroll") for (i = 0; i < UNROLL; i++)                  \
-                {                                                               \
-                    tmp1[i] = _OP(tmp1[i], tmp2[i]);                            \
-                }                                                               \
-            }                                                                   \
-            if (flags & UCC_EEE_TASK_FLAG_REDUCE_WITH_ALPHA) {                  \
-                _Pragma("unroll") for (i = 0; i < UNROLL; i++)                  \
-                {                                                               \
-                    tmp1[i] = tmp1[i] * (_AlphaType)task.alpha;                 \
-                }                                                               \
-            }                                                                   \
-            _Pragma("unroll") for (i = 0; i < UNROLL; i++)                      \
-            {                                                                   \
-                d[line + WARP_SIZE * i] = tmp1[i];                              \
-            }                                                                   \
-        }                                                                       \
-        for (i = triggered                                                      \
-                     ? num_lines + threadIdx.x                                  \
-                     : num_lines + threadIdx.x + blockIdx.x * blockDim.x;       \
-             i < count;                                                         \
-             i += triggered ? blockDim.x : blockDim.x * gridDim.x) {            \
-            d[i] = _OP(s[0][i], s[1][i]);                                       \
-            for (j = 2; j < UCC_EE_EXECUTOR_NUM_BUFS; j++) {                    \
-                if (j >= n_srcs) {                                              \
-                    break;                                                      \
-                }                                                               \
-                d[i] = _OP(d[i], s[j][i]);                                      \
-            }                                                                   \
-        }                                                                       \
-        if (flags & UCC_EEE_TASK_FLAG_REDUCE_WITH_ALPHA) {                      \
-            for (i = triggered                                                  \
-                         ? num_lines + threadIdx.x                              \
-                         : num_lines + threadIdx.x + blockIdx.x * blockDim.x;   \
-                 i < count;                                                     \
-                 i += triggered ? blockDim.x : blockDim.x * gridDim.x) {        \
-                d[i] = d[i] * (_AlphaType)task.alpha;                           \
-            }                                                                   \
-        }                                                                       \
-    }                                                                           \
-    template <typename _Type, typename _AlphaType, bool triggered, int UNROLL>  \
-    __global__ void UCC_REDUCE_CUDA_DEFAULT_##NAME(ucc_eee_task_reduce_t task,  \
-                                                   uint16_t              flags) \
-    {                                                                           \
-        ucc_reduce_cuda_default_##NAME<_Type, _AlphaType, triggered, UNROLL>(   \
-            task, flags);                                                       \
+                           : (threadIdx.x + blockIdx.x * blockDim.x) / WARP_SIZE; \
+        const int    num_warps = triggered                                        \
+                                     ? blockDim.x / WARP_SIZE                     \
+                                     : (blockDim.x * gridDim.x) / WARP_SIZE;      \
+        const int    idx       = threadIdx.x % WARP_SIZE;                         \
+        const size_t num_lines =                                                  \
+            (count / (WARP_SIZE * UNROLL)) * (WARP_SIZE * UNROLL);                \
+        const _Type *s[UCC_EE_EXECUTOR_NUM_BUFS];                                 \
+        _Type        tmp1[UNROLL];                                                \
+        _Type        tmp2[UNROLL];                                                \
+        size_t       i, j;                                                        \
+        memcpy(s, task.srcs, UCC_EE_EXECUTOR_NUM_BUFS * sizeof(_Type *));         \
+        for (size_t line = warp * WARP_SIZE * UNROLL + idx; line < num_lines;     \
+             line += num_warps * WARP_SIZE * UNROLL) {                            \
+            _Pragma("unroll") for (i = 0; i < UNROLL; i++)                        \
+            {                                                                     \
+                tmp1[i] = s[0][line + WARP_SIZE * i];                             \
+            }                                                                     \
+            for (j = 1; j < UCC_EE_EXECUTOR_NUM_BUFS; j++) {                      \
+                if (j >= n_srcs) {                                                \
+                    break;                                                        \
+                }                                                                 \
+                _Pragma("unroll") for (i = 0; i < UNROLL; i++)                    \
+                {                                                                 \
+                    tmp2[i] = s[j][line + WARP_SIZE * i];                         \
+                }                                                                 \
+                _Pragma("unroll") for (i = 0; i < UNROLL; i++)                    \
+                {                                                                 \
+                    tmp1[i] = _OP(tmp1[i], tmp2[i]);                              \
+                }                                                                 \
+            }                                                                     \
+            if (flags & UCC_EEE_TASK_FLAG_REDUCE_WITH_ALPHA) {                    \
+                _Pragma("unroll") for (i = 0; i < UNROLL; i++)                    \
+                {                                                                 \
+                    tmp1[i] = tmp1[i] * (_AlphaType)task.alpha;                   \
+                }                                                                 \
+            }                                                                     \
+            _Pragma("unroll") for (i = 0; i < UNROLL; i++)                        \
+            {                                                                     \
+                d[line + WARP_SIZE * i] = tmp1[i];                                \
+            }                                                                     \
+        }                                                                         \
+        for (i = triggered                                                        \
+                     ? num_lines + threadIdx.x                                    \
+                     : num_lines + threadIdx.x + blockIdx.x * blockDim.x;         \
+             i < count;                                                           \
+             i += triggered ? blockDim.x : blockDim.x * gridDim.x) {              \
+            d[i] = _OP(s[0][i], s[1][i]);                                         \
+            for (j = 2; j < UCC_EE_EXECUTOR_NUM_BUFS; j++) {                      \
+                if (j >= n_srcs) {                                                \
+                    break;                                                        \
+                }                                                                 \
+                d[i] = _OP(d[i], s[j][i]);                                        \
+            }                                                                     \
+        }                                                                         \
+        if (flags & UCC_EEE_TASK_FLAG_REDUCE_WITH_ALPHA) {                        \
+            for (i = triggered                                                    \
+                         ? num_lines + threadIdx.x                                \
+                         : num_lines + threadIdx.x + blockIdx.x * blockDim.x;     \
+                 i < count;                                                       \
+                 i += triggered ? blockDim.x : blockDim.x * gridDim.x) {          \
+                d[i] = d[i] * (_AlphaType)task.alpha;                             \
+            }                                                                     \
+        }                                                                         \
+    }                                                                             \
+    template <typename _Type, typename _AlphaType, bool triggered, int UNROLL>    \
+    __global__ void UCC_REDUCE_CUDA_DEFAULT_##NAME(ucc_eee_task_reduce_t task,    \
+                                                   uint16_t              flags)   \
+    {                                                                             \
+        ucc_reduce_cuda_default_##NAME<_Type, _AlphaType, triggered, UNROLL>(     \
+            task, flags);                                                         \
     }
 
 #define CUDA_REDUCE_WITH_OP_STRIDED(NAME, _OP)                                  \
@@ -161,10 +161,10 @@ cuFloatComplex operator* (const cuFloatComplex & first,
         const int    idx       = threadIdx.x % WARP_SIZE;                       \
         const size_t num_lines =                                                \
             (count / (WARP_SIZE * UNROLL)) * (WARP_SIZE * UNROLL);              \
-        __shared__ uint16_t n_src2;                                                  \
-        _Type          tmp1[UNROLL];                                            \
-        _Type          tmp2[UNROLL];                                            \
-        size_t         i, j;                                                    \
+        __shared__ uint16_t n_src2;                                             \
+        _Type               tmp1[UNROLL];                                       \
+        _Type               tmp2[UNROLL];                                       \
+        size_t              i, j;                                               \
         n_src2 = task.n_src2;                                                   \
         ucc_assert(task.stride % sizeof(_Type) == 0);                           \
         for (size_t line = warp * WARP_SIZE * UNROLL + idx; line < num_lines;   \
@@ -173,7 +173,7 @@ cuFloatComplex operator* (const cuFloatComplex & first,
             {                                                                   \
                 tmp1[i] = s1[line + WARP_SIZE * i];                             \
             }                                                                   \
-            for (j = 0; j < USHRT_MAX; j++) {                                     \
+            for (j = 0; j < USHRT_MAX; j++) {                                   \
                 if (j >= n_src2) {                                              \
                     break;                                                      \
                 }                                                               \
@@ -203,7 +203,7 @@ cuFloatComplex operator* (const cuFloatComplex & first,
              i < count;                                                         \
              i += triggered ? blockDim.x : blockDim.x * gridDim.x) {            \
             d[i] = _OP(s1[i], s2[i]);                                           \
-            for (j = 1; j < USHRT_MAX; j++) {                                     \
+            for (j = 1; j < USHRT_MAX; j++) {                                   \
                 if (j >= n_src2) {                                              \
                     break;                                                      \
                 }                                                               \
