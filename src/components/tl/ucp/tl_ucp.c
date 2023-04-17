eae1a2e97d536c89edabcec2454d9fe22600ed44
Sergey Lebedev <sergeyle@nvidia.com>
2023-04-17 10:49:19 +0000

    TL/UCP: avoid copy in knomial scatter

--- src/components/tl/ucp/tl_ucp.c
+++ src/components/tl/ucp/tl_ucp.c
@@ -150,6 +150,11 @@ ucc_config_field_t ucc_tl_ucp_lib_config_table[] = {
      ucc_offsetof(ucc_tl_ucp_lib_config_t, scatter_kn_radix),
      UCC_CONFIG_TYPE_UINT},
 
+    {"SCATTER_KN_ENABLE_RECV_ZCOPY", "auto",
+     "Receive scatter data to user buffer with correct offset using zcopy",
+     ucs_offsetof(ucc_tl_ucp_lib_config_t, scatter_kn_enable_recv_zcopy),
+     UCS_CONFIG_TYPE_ON_OFF_AUTO},
+
     {"SCATTERV_LINEAR_NUM_POSTS", "16",
      "Maximum number of outstanding send and receive messages in scatterv "
      "linear algorithm",
