17a922c63adcdf1854819125ed018def1e4684da
samnordmann <snordmann@nvidia.com>
2023-05-23 19:09:44 +0300

    TL/MLX5: rcache (#753)
    * TL/MLX5: rcache
    
    * CODESTYLE: clang-tidy
    
    * TL/MLX5: fixes and  Linter errors
    
    * TL/MLX5: minor reviews
    
    * TL/MLX5: minor reviews
    
    * TL/MLX5: minor reviews
    
    * TL/MLX5: revisions
    
    * TL/MLX5: fix Linter error
    
    ---------
    
    Co-authored-by: Valentin Petrov <valentinp@nvidia.com>

--- src/components/tl/mlx5/tl_mlx5.h
+++ src/components/tl/mlx5/tl_mlx5.h
@@ -97,6 +97,17 @@ typedef struct ucc_tl_mlx5_team {
 UCC_CLASS_DECLARE(ucc_tl_mlx5_team_t, ucc_base_context_t *,
                   const ucc_base_team_params_t *);
 
+ucc_status_t tl_mlx5_rcache_create(ucc_tl_mlx5_context_t *ctx);
+
+typedef struct ucc_tl_mlx5_reg {
+    struct ibv_mr       *mr;
+} ucc_tl_mlx5_reg_t;
+
+typedef struct ucc_tl_mlx5_rcache_region {
+    ucc_rcache_region_t super;
+    ucc_tl_mlx5_reg_t   reg;
+} ucc_tl_mlx5_rcache_region_t;
+
 #define UCC_TL_MLX5_SUPPORTED_COLLS (UCC_COLL_TYPE_ALLTOALL)
 
 #define UCC_TL_MLX5_TEAM_LIB(_team)                                            \
