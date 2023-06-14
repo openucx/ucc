4a816302240e003d3e11d8395b274c2cecf6a88f
Manjunath Gorentla Venkata <manjugv@gmail.com>
2023-06-14 09:13:51 -0700

    DOCS: Adding v1.2 documentation

---
+++ api/v1.2/v1.2/html/search/pages_4.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['teams_0',['Teams',['../md_docs_doxygen_teams.html',1,'']]],
+  ['types_20of_20collective_20operations_1',['Types of Collective Operations',['../md_docs_doxygen_collectives.html',1,'']]]
+];
