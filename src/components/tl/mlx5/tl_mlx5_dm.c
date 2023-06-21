5917644b989127c6c79666dd60b19a6324578b4e
snordmann <snordmann@nvidia.com>
2023-06-21 17:55:12 +0300

    TL/MLX5: support rectangular blocks
    clean and working
    
    TL/MLX5: add more config for block dimensions
    
    force longer by default

--- src/components/tl/mlx5/tl_mlx5_dm.c
+++ src/components/tl/mlx5/tl_mlx5_dm.c
@@ -236,7 +236,6 @@ ucc_status_t ucc_tl_mlx5_dm_init(ucc_tl_mlx5_team_t *team)
         goto err_dm_alloc;
     }
     team->dm_offset = 0;
-    printf("Number of MEMIC chunks = %ld, chunck size = %ld\n", cfg->dm_buf_num, cfg->dm_buf_size  * cfg->block_batch_size);
     // TODO: fix case dm_host=true
     status = ucc_mpool_init(&team->dm_pool, 0, sizeof(ucc_tl_mlx5_dm_chunk_t),
                             0, UCC_CACHE_LINE_SIZE, 1,
