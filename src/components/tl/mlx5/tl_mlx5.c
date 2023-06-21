b8fe903e7a4b7398fec9322eff127ccd32e59c9f
snordmann <snordmann@nvidia.com>
2023-06-21 17:55:12 +0300

    TL/MLX5: support rectangular blocks
    clean and working
    
    TL/MLX5: add more config for block dimensions
    
    force longer by default

--- src/components/tl/mlx5/tl_mlx5.c
+++ src/components/tl/mlx5/tl_mlx5.c
@@ -28,6 +28,16 @@ static ucc_config_field_t ucc_tl_mlx5_lib_config_table[] = {
      ucc_offsetof(ucc_tl_mlx5_lib_config_t, dm_buf_num),
      UCC_CONFIG_TYPE_ULUNITS},
 
+    {"FORCE_REGULAR", "y", "Force the regular case where the block dimensions "
+    "divide ppn. Requires BLOCK_SIZE=0",
+     ucc_offsetof(ucc_tl_mlx5_lib_config_t, force_regular), UCC_CONFIG_TYPE_BOOL},
+
+    {"FORCE_LONGER", "y", "Force the blocks to have more height than width",
+     ucc_offsetof(ucc_tl_mlx5_lib_config_t, force_longer), UCC_CONFIG_TYPE_BOOL},
+
+    {"FORCE_WIDER", "n", "Force the blocks to have more width than height",
+     ucc_offsetof(ucc_tl_mlx5_lib_config_t, force_wider), UCC_CONFIG_TYPE_BOOL},
+
     {"BLOCK_SIZE", "0",
      "Size of the blocks that are sent using blocked AlltoAll Algorithm",
      ucc_offsetof(ucc_tl_mlx5_lib_config_t, block_size), UCC_CONFIG_TYPE_UINT},
@@ -108,13 +118,13 @@ static ucc_config_field_t ucc_tl_mlx5_lib_config_table[] = {
      ucc_offsetof(ucc_tl_mlx5_lib_config_t, fanin_kn_radix),
      UCC_CONFIG_TYPE_UINT},
 
-    {"SEND_BATCH_SIZE", "8", "number of blocks that are transposed "
+    {"SEND_BATCH_SIZE", "1", "number of blocks that are transposed "
     "on the NIC before being sent as a batch to a remote peer",
      ucc_offsetof(ucc_tl_mlx5_lib_config_t, block_batch_size),
      UCC_CONFIG_TYPE_UINT},
 
-    {"NBR_SERIALIZED_BATCHES", "2", "number of block batches "
-    "(within the set of blocks to be sent to a given remote peer)"
+    {"NBR_SERIALIZED_BATCHES", "1", "number of block batches "
+    "(within the set of blocks to be sent to a given remote peer) "
      "serialized on the same device memory chunk",
      ucc_offsetof(ucc_tl_mlx5_lib_config_t, nbr_serialized_batches),
      UCC_CONFIG_TYPE_UINT},
