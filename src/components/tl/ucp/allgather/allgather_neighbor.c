3a185aa8cc152bcea2540c3cd81a885961b8df8d
Ilya Kryukov <ilyakrukov@gmail.com>
2023-08-21 16:46:33 +0200

    TL/UCP: formatting using clangformat

--- src/components/tl/ucp/allgather/allgather_neighbor.c
+++ src/components/tl/ucp/allgather/allgather_neighbor.c
@@ -7,18 +7,14 @@
 #include "utils/ucc_coll_utils.h"
 #include "components/mc/ucc_mc.h"
 
-static ucc_rank_t ucc_tl_ucp_allgather_neighbor_get_send_block(ucc_subset_t *subset,
-                                                           ucc_rank_t trank,
-                                                           ucc_rank_t tsize,
-                                                           int step)
+static ucc_rank_t ucc_tl_ucp_allgather_neighbor_get_send_block(
+    ucc_subset_t *subset, ucc_rank_t trank, ucc_rank_t tsize, int step)
 {
     return ucc_ep_map_eval(subset->map, (trank - step + tsize) % tsize);
 }
 
-static ucc_rank_t ucc_tl_ucp_allgather_neighbor_get_recv_block(ucc_subset_t *subset,
-                                                           ucc_rank_t trank,
-                                                           ucc_rank_t tsize,
-                                                           int step)
+static ucc_rank_t ucc_tl_ucp_allgather_neighbor_get_recv_block(
+    ucc_subset_t *subset, ucc_rank_t trank, ucc_rank_t tsize, int step)
 {
     return ucc_ep_map_eval(subset->map, (trank - step - 1 + tsize) % tsize);
 }
@@ -29,33 +25,34 @@ static ucc_rank_t get_recv_from_rank(ucc_rank_t rank, ucc_rank_t size, int i)
     int even_rank;
     even_rank = !(rank % 2);
     if (even_rank) {
-        neighbor[0] = (rank + 1) % size;
-        neighbor[1] = (rank - 1 + size) % size;
+        neighbor[0]       = (rank + 1) % size;
+        neighbor[1]       = (rank - 1 + size) % size;
         recv_data_from[0] = rank;
         recv_data_from[1] = rank;
         offset_at_step[0] = (+2);
         offset_at_step[1] = (-2);
     } else {
-        neighbor[0] = (rank - 1 + size) % size;
-        neighbor[1] = (rank + 1) % size; 
-        recv_data_from[0] = neighbor[0]; 
-        recv_data_from[1] = neighbor[0]; 
+        neighbor[0]       = (rank - 1 + size) % size;
+        neighbor[1]       = (rank + 1) % size;
+        recv_data_from[0] = neighbor[0];
+        recv_data_from[1] = neighbor[0];
         offset_at_step[0] = (-2);
         offset_at_step[1] = (+2);
     }
     const int i_parity = i % 2;
-    return (recv_data_from[i_parity] + offset_at_step[i_parity] * ((i + 1) / 2) + size) % size;
+    return (recv_data_from[i_parity] +
+            offset_at_step[i_parity] * ((i + 1) / 2) + size) %
+           size;
 }
 
-
 ucc_status_t ucc_tl_ucp_allgather_neighbor_init(ucc_base_coll_args_t *coll_args,
-                                            ucc_base_team_t *     team,
-                                            ucc_coll_task_t **    task_h)
+                                                ucc_base_team_t      *team,
+                                                ucc_coll_task_t     **task_h)
 {
     ucc_tl_ucp_task_t *task;
-    ucc_status_t status;
+    ucc_status_t       status;
 
-    task = ucc_tl_ucp_init_task(coll_args, team);
+    task   = ucc_tl_ucp_init_task(coll_args, team);
     status = ucc_tl_ucp_allgather_neighbor_init_common(task);
     if (status != UCC_OK) {
         ucc_tl_ucp_put_task(task);
@@ -66,21 +63,21 @@ ucc_status_t ucc_tl_ucp_allgather_neighbor_init(ucc_base_coll_args_t *coll_args,
 
 ucc_status_t ucc_tl_ucp_allgather_neighbor_init_common(ucc_tl_ucp_task_t *task)
 {
-    ucc_tl_ucp_team_t *team       = TASK_TEAM(task);
-    ucc_rank_t         tsize      = UCC_TL_TEAM_SIZE(team);
-    ucc_sbgp_t *sbgp;
+    ucc_tl_ucp_team_t *team  = TASK_TEAM(task);
+    ucc_rank_t         tsize = UCC_TL_TEAM_SIZE(team);
+    ucc_sbgp_t        *sbgp;
 
     if (!ucc_coll_args_is_predefined_dt(&TASK_ARGS(task), UCC_RANK_INVALID)) {
         tl_error(UCC_TASK_LIB(task), "user defined datatype is not supported");
         return UCC_ERR_NOT_SUPPORTED;
     }
 
-    if (tsize % 2)
-    {
-        tl_warn(UCC_TASK_LIB(task), "odd team size is not supported, switching to ring");
+    if (tsize % 2) {
+        tl_warn(UCC_TASK_LIB(task),
+                "odd team size is not supported, switching to ring");
         return ucc_tl_ucp_allgather_ring_init_common(task);
     }
-    
+
     if (!(task->flags & UCC_TL_UCP_TASK_FLAG_SUBSET)) {
         if (team->cfg.use_reordering) {
             sbgp = ucc_topo_get_sbgp(team->topo, UCC_SBGP_FULL_HOST_ORDERED);
@@ -89,27 +86,29 @@ ucc_status_t ucc_tl_ucp_allgather_neighbor_init_common(ucc_tl_ucp_task_t *task)
         }
     }
 
-    task->allgather_neighbor.get_send_block = ucc_tl_ucp_allgather_neighbor_get_send_block;
-    task->allgather_neighbor.get_recv_block = ucc_tl_ucp_allgather_neighbor_get_recv_block;
-    task->super.post                    = ucc_tl_ucp_allgather_neighbor_start;
-    task->super.progress                = ucc_tl_ucp_allgather_neighbor_progress;
+    task->allgather_neighbor.get_send_block =
+        ucc_tl_ucp_allgather_neighbor_get_send_block;
+    task->allgather_neighbor.get_recv_block =
+        ucc_tl_ucp_allgather_neighbor_get_recv_block;
+    task->super.post     = ucc_tl_ucp_allgather_neighbor_start;
+    task->super.progress = ucc_tl_ucp_allgather_neighbor_progress;
 
     return UCC_OK;
 }
 
 /* Original implmenetation: https://github.com/open-mpi/ompi/blob/main/ompi/mca/coll/base/coll_base_allgather.c */
-void  ucc_tl_ucp_allgather_neighbor_progress(ucc_coll_task_t *coll_task)
+void ucc_tl_ucp_allgather_neighbor_progress(ucc_coll_task_t *coll_task)
 {
-    ucc_tl_ucp_task_t *task       = ucc_derived_of(coll_task, ucc_tl_ucp_task_t);
-    ucc_tl_ucp_team_t *team       = TASK_TEAM(task);
-    ucc_rank_t         trank      = UCC_TL_TEAM_RANK(team);
-    ucc_rank_t         tsize      = UCC_TL_TEAM_SIZE(team);
+    ucc_tl_ucp_task_t *task  = ucc_derived_of(coll_task, ucc_tl_ucp_task_t);
+    ucc_tl_ucp_team_t *team  = TASK_TEAM(task);
+    ucc_rank_t         trank = UCC_TL_TEAM_RANK(team);
+    ucc_rank_t         tsize = UCC_TL_TEAM_SIZE(team);
     ucc_rank_t         neighbor[2];
-    void              *rbuf       = TASK_ARGS(task).dst.info.buffer;
-    ucc_memory_type_t  rmem       = TASK_ARGS(task).dst.info.mem_type;
-    size_t             count      = TASK_ARGS(task).dst.info.count;
-    ucc_datatype_t     dt         = TASK_ARGS(task).dst.info.datatype;
-    size_t             data_size  = (count / tsize) * ucc_dt_size(dt);
+    void              *rbuf      = TASK_ARGS(task).dst.info.buffer;
+    ucc_memory_type_t  rmem      = TASK_ARGS(task).dst.info.mem_type;
+    size_t             count     = TASK_ARGS(task).dst.info.count;
+    ucc_datatype_t     dt        = TASK_ARGS(task).dst.info.datatype;
+    size_t             data_size = (count / tsize) * ucc_dt_size(dt);
     ucc_rank_t         i;
     int                even_rank;
     void              *tmprecv, *tmpsend;
@@ -128,30 +127,33 @@ void  ucc_tl_ucp_allgather_neighbor_progress(ucc_coll_task_t *coll_task)
     }
 
     while (task->tagged.send_posted < (tsize / 2)) {
-        i = task->tagged.send_posted;
+        i                  = task->tagged.send_posted;
         const int i_parity = i % 2;
-       
-        tmprecv = PTR_OFFSET(rbuf, get_recv_from_rank(trank, tsize, i) * data_size);
-        tmpsend = PTR_OFFSET(rbuf, get_recv_from_rank(trank, tsize, i - 1) * data_size);
+
+        tmprecv =
+            PTR_OFFSET(rbuf, get_recv_from_rank(trank, tsize, i) * data_size);
+        tmpsend = PTR_OFFSET(rbuf, get_recv_from_rank(trank, tsize, i - 1) *
+                                       data_size);
 
         /* Sendreceive */
-        UCPCHECK_GOTO(
-            ucc_tl_ucp_send_nb(tmpsend, 2 * data_size, rmem, neighbor[i_parity], team, task),
-            task, out);
-        UCPCHECK_GOTO(
-            ucc_tl_ucp_recv_nb(tmprecv, 2 * data_size, rmem, neighbor[i_parity], team, task),
-            task, out);
-        
+        UCPCHECK_GOTO(ucc_tl_ucp_send_nb(tmpsend, 2 * data_size, rmem,
+                                         neighbor[i_parity], team, task),
+                      task, out);
+        UCPCHECK_GOTO(ucc_tl_ucp_recv_nb(tmprecv, 2 * data_size, rmem,
+                                         neighbor[i_parity], team, task),
+                      task, out);
+
         if (UCC_INPROGRESS == ucc_tl_ucp_test(task)) {
             return;
         }
     }
-    
+
     ucc_assert(UCC_TL_UCP_TASK_P2P_COMPLETE(task));
     task->super.status = UCC_OK;
 
 out:
-    UCC_TL_UCP_PROFILE_REQUEST_EVENT(coll_task, "ucp_allgather_neighbor_done", 0);
+    UCC_TL_UCP_PROFILE_REQUEST_EVENT(coll_task, "ucp_allgather_neighbor_done",
+                                     0);
 }
 
 ucc_status_t ucc_tl_ucp_allgather_neighbor_start(ucc_coll_task_t *coll_task)
@@ -173,13 +175,14 @@ ucc_status_t ucc_tl_ucp_allgather_neighbor_start(ucc_coll_task_t *coll_task)
     ucc_rank_t         neighbor[2];
     void              *tmprecv, *tmpsend;
 
-    UCC_TL_UCP_PROFILE_REQUEST_EVENT(coll_task, "ucp_allgather_neighbor_start", 0);
+    UCC_TL_UCP_PROFILE_REQUEST_EVENT(coll_task, "ucp_allgather_neighbor_start",
+                                     0);
     ucc_tl_ucp_task_reset(task, UCC_INPROGRESS);
 
     if (!UCC_IS_INPLACE(TASK_ARGS(task))) {
-        block = trank % tsize;
-        status = ucc_mc_memcpy(PTR_OFFSET(rbuf, data_size * block),
-                               sbuf, data_size, rmem, smem);
+        block  = trank % tsize;
+        status = ucc_mc_memcpy(PTR_OFFSET(rbuf, data_size * block), sbuf,
+                               data_size, rmem, smem);
         if (ucc_unlikely(UCC_OK != status)) {
             return status;
         }
