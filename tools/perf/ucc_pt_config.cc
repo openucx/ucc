54d3668117d4e54ea128ef6b6a0923287484ff7c
Sergey Lebedev <sergeyle@nvidia.com>
2023-10-12 10:41:38 +0000

    TOOLS: add cuda managed mtype to perftest

--- tools/perf/ucc_pt_config.cc
+++ tools/perf/ucc_pt_config.cc
@@ -60,6 +60,7 @@ const std::map<std::string, ucc_memory_type_t> ucc_pt_memtype_map = {
     {"host", UCC_MEMORY_TYPE_HOST},
     {"cuda", UCC_MEMORY_TYPE_CUDA},
     {"rocm", UCC_MEMORY_TYPE_ROCM},
+    {"cuda-mng", UCC_MEMORY_TYPE_CUDA_MANAGED},
 };
 
 const std::map<std::string, ucc_datatype_t> ucc_pt_datatype_map = {
