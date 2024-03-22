f3f1cf945e55011be8fa7f981ce277efc9f5d035
Ilya Kryukov <ilyakrukov@gmail.com>
2024-03-22 11:59:58 +0100

    TL/CUDA: fix build

--- src/components/tl/cuda/bcast/bcast_linear.c
+++ src/components/tl/cuda/bcast/bcast_linear.c
@@ -29,7 +29,8 @@ void ucc_tl_cuda_bcast_linear_progress(ucc_coll_task_t *coll_task)
     ucc_tl_cuda_task_t *task = ucc_derived_of(coll_task, ucc_tl_cuda_task_t);
     ucc_tl_cuda_team_t *team = TASK_TEAM(task);
     ucc_status_t        st;
-
+    (void) team;
+    (void) st;
     task->super.status = UCC_INPROGRESS;
 }
 
@@ -40,11 +41,12 @@ ucc_status_t ucc_tl_cuda_bcast_linear_start(ucc_coll_task_t *coll_task)
     ucc_coll_args_t *   args  = &TASK_ARGS(task);
     ucc_rank_t          tsize = UCC_TL_TEAM_SIZE(team);
     ucc_datatype_t      dt    = task->allgatherv_linear.dt;
-    ucc_rank_t          i;
-    size_t              send_size, frag_size, ssize;
 
+    (void) tsize;
+    (void) args;
+    (void) dt;
     task->bcast_linear.stage         = STAGE_SYNC;
-    task->allgatherv_linear.sbuf          = args->src.info.buffer;
+    // task->bcast_linear.sbuf          = args->src.info.buffer;
 
 
     return ucc_progress_queue_enqueue(UCC_TL_CORE_CTX(team)->pq, &task->super);
@@ -75,9 +77,9 @@ ucc_status_t ucc_tl_cuda_bcast_linear_init(ucc_base_coll_args_t *coll_args,
     // task->allgatherv_linear.rbuf       = coll_args->args.dst.info.buffer;
 
     task->super.flags |= UCC_COLL_TASK_FLAG_EXECUTOR;
-    task->super.post           = ucc_tl_cuda_allgatherv_linear_start;
-    task->super.progress       = ucc_tl_cuda_allgatherv_linear_progress;
-    task->super.finalize       = ucc_tl_cuda_allgatherv_linear_finalize;
+    task->super.post           = ucc_tl_cuda_bcast_linear_start;
+    task->super.progress       = ucc_tl_cuda_bcast_linear_progress;
+    task->super.finalize       = ucc_tl_cuda_bcast_linear_finalize;
     task->bar                  = TASK_BAR(task);
 
     *task_p = &task->super;
