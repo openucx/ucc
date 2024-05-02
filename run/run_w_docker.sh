62c780a96dd9c5359bc4dbdee612631f9d55caa1
Eyal Chocron <x41lakazam@gmail.com>
2024-05-02 19:05:20 +0300

    rm useless directory

--- run/run_w_docker.sh
+++
@@ -1,5 +0,0 @@
-#source ./run/env.sh
-
-UCC_PT_COLL_ALLTOALLV_TRANSFER_MATRIX_FILE="/opt/test_data/transfer_matrix_8" UCX_IB_NUM_PATHS=4 UCX_TLS=rc,cuda_copy UCX_RNDV_SCHEME=get_zcopy MELLANOX_VISIBLE_DEVICES=0,3,4,5,6,9,10,11 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 UCX_IB_GID_INDEX=3  UCC_CLS=basic UCX_RNDV_THRESH=0 UCC_TLS=ucp OMPI_MCA_btl=tcp,self OMPI_MCA_btl_tcp_if_include=eno8303 srun -p ISR1-ALL -N 1 --ntasks-per-node=8 --gpus-per-node=8 --mpi=pmix --container-image=$PWD/container/ucc_transfer_matrix.sqsh --container-mounts=$PWD/run/test_matrices:/opt/test_data /opt/ucc/build/install/bin/ucc_perftest -c alltoallv 
-
-
--- run/shimmy.sh
+++
@@ -1,3 +0,0 @@
-source run/env.sh
-
-nnodes=1; ppn=8; mpirun -np $((nnodes*ppn)) -x UCC_TL_NCCL_TUNE=0 -x UCC_MC_CUDA_MPOOL_MAX_ELEMS=128 -x UCC_MC_CUDA_MPOOL_ELEM_SIZE=1048576 --bind-to core ./build-original/bin/ucc_perftest -c alltoallv -b 128 -e 1048576
