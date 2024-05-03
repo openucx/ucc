0bf857d08a4609f92462c7949650f66f240dddb5
samnordmann <snordmann@nvidia.com>
2024-05-03 10:44:39 +0200

    TL/MLX5: ci (#806)
    * TL/MLX5: set up ci
    
    * TL/MLX5: adding ci check that nnodes>2
    
    * TL/MLX5: fix bug
    
    * TL/MLX5: add port
    
    * TL/MLX5: add debug print for ci
    
    * TL/MLX5: simplify CI test to debug
    
    * TL/MLX5: deactivate mcast bcast
    
    * TL/MLX5: deactivate a2a also
    
    * TL/MLX5: deactivating a2a only
    
    * TL/MLX5: CI cleaning
    
    * TL/MLX5: minor revisions in CI
    
    * TL/MLX5: CI minor comments
    
    * TL/MLX5: remove setting UCX's dm to zero
    
    * TL/MLX5: add tls in cmake config
    
    * TL/MLX5: empty commit to trigger CI

--- .ci/scripts/build_ucc.sh
+++ .ci/scripts/build_ucc.sh
@@ -8,7 +8,8 @@ cd "${UCC_SRC_DIR}"
 mkdir -p "${UCC_SRC_DIR}/build"
 cd "${UCC_SRC_DIR}/build"
 "${UCC_SRC_DIR}/configure" --with-ucx="${UCX_INSTALL_DIR}" --with-cuda="${CUDA_HOME}" \
-    --prefix="${UCC_INSTALL_DIR}" --enable-gtest --with-mpi
+    --prefix="${UCC_INSTALL_DIR}" --enable-gtest --with-mpi \
+    --with-tls=cuda,nccl,self,sharp,shm,ucp,mlx5
 make -j install
 echo "${UCC_INSTALL_DIR}/lib" > /etc/ld.so.conf.d/ucc.conf
 ldconfig
--- .ci/scripts/run_tests_ucc_mpi.sh
+++ .ci/scripts/run_tests_ucc_mpi.sh
@@ -21,13 +21,20 @@ export MASTER_ADDR=${HEAD_NODE}
 
 NNODES=$(wc --lines "$HOSTFILE" | awk '{print $1}')
 DEV=""
+CX7_DEV=""
 
 # Find first available active device
 for d in $(ssh $HEAD_NODE "ibstat -l"); do
     state=$(ssh $HEAD_NODE "ibstat $d" | grep 'State:' | awk '{print $2}')
+    type=$(ssh $HEAD_NODE "ibstat $d" | grep 'CA type:' | awk '{print $3}')
     if [ $state == 'Active' ]; then
-        DEV=$d
-        break
+        if [ "$DEV" == "" ]; then
+            DEV=$d
+        fi
+        if [ "$type" == 'MT4129' ]; then
+            CX7_DEV=$d
+            break
+        fi
     fi
 done
 
@@ -101,6 +108,20 @@ for MT in "" "-T"; do
     mpirun $(mpi_params $PPN 1) $ucx_tls_no_cuda_ipc $tlcuda_args $EXE $MT $TG --mtypes cuda -c $tlcuda_colls
     echo "INFO: UCC MPI unit tests (TL/CUDA) ... DONE"
 
+    echo "INFO: UCC MPI unit tests (TL/MLX5) ..."
+    # shellcheck disable=SC2086
+    if [ "x$CX7_DEV" == "x" ]; then
+        echo "WARNING: No active CX7 devices found on ${HEAD_NODE}"
+        echo "INFO: UCC MPI unit tests (TL/MLX5) ... SKIPPED"
+    elif [ $NNODES -lt 2 ]; then
+        echo "WARNING: At least two nodes are required, but only $NNODES are available"
+        echo "INFO: UCC MPI unit tests (TL/MLX5) ... SKIPPED"
+    else
+        tlmlx5_args=" -x UCC_CLS=basic -x UCC_CL_BASIC_TLS=ucp,mlx5 -x UCC_TL_MLX5_NET_DEVICES=$CX7_DEV:1 -x UCC_TL_MLX5_TUNE=inf "
+        tlmlx5_colls="alltoall"
+        mpirun $(mpi_params $PPN) $tlmlx5_args $EXE $MT $TG --mtypes host,cuda -c $tlmlx5_colls -t world -d uint8 -O 0 -m 1:128
+        echo "INFO: UCC MPI unit tests (TL/MLX5) ... DONE"
+    fi
 
     echo "INFO: UCC MPI unit tests (CL/HIER) ..."
     # shellcheck disable=SC2086
