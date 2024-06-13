3175f2d2a208518db0899f4e4229c9b95c416fa5
Nick Sarkauskas <nsarkauskas@nvidia.com>
2024-06-13 08:08:27 -0700

    TL/UCP: Add xgvmi allgather

--- src/components/tl/ucp/allgather/allgather.c
+++ src/components/tl/ucp/allgather/allgather.c
@@ -31,6 +31,10 @@ ucc_base_coll_alg_info_t
             {.id   = UCC_TL_UCP_ALLGATHER_ALG_SPARBIT,
              .name = "sparbit",
              .desc = "O(log(N)) SPARBIT algorithm"},
+        [UCC_TL_UCP_ALLGATHER_ALG_XGVMI] =
+            {.id   = UCC_TL_UCP_ALLGATHER_ALG_XGVMI,
+             .name = "xgvmi",
+             .desc = "Offloaded xgvmi algorithm"},
         [UCC_TL_UCP_ALLGATHER_ALG_LAST] = {
             .id = 0, .name = NULL, .desc = NULL}};
 
