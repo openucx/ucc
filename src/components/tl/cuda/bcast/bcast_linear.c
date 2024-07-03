6233440d40926cabf40b2aa230cdb72553fadce4
Ilya Kryukov <ilyakrukov@gmail.com>
2024-07-03 13:20:13 +0200

    TL/CUDA: added barrier to sync stages

--- src/components/tl/cuda/bcast/bcast_linear.c
+++ src/components/tl/cuda/bcast/bcast_linear.c
@@ -6,9 +6,10 @@
 
 #include "bcast/bcast.h"
 
-enum
-{
+enum {
     STAGE_DONE,
+    STAGE_SYNC,
+    STAGE_SETUP,
     // root
     STAGE_COPY,      // post copy task: copy block from src to scratch buffer
     STAGE_WAIT_COPY, // wait for copy finishes
@@ -36,8 +37,33 @@ static inline void set_rank_step(ucc_tl_cuda_task_t *task, ucc_rank_t rank,
     sync->seq_num[step_id] = step;
 }
 
+ucc_status_t ucc_tl_cuda_bcast_linear_setup_start(ucc_tl_cuda_task_t *task)
+{
+    ucc_tl_cuda_team_t *team  = TASK_TEAM(task);
+    ucc_rank_t          trank = UCC_TL_TEAM_RANK(team);
+    ucc_status_t        status;
+
+    set_rank_step(task, trank, 0, 0);
+    ucc_memory_cpu_store_fence();
+    status = ucc_tl_cuda_shm_barrier_start(UCC_TL_TEAM_RANK(team), task->bar);
+    if (ucc_unlikely(status != UCC_OK)) {
+        goto exit_err;
+    }
+
+    return UCC_OK;
+
+exit_err:
+    return status;
+}
+
+ucc_status_t ucc_tl_cuda_bcast_linear_setup_test(ucc_tl_cuda_task_t *task)
+{
+    ucc_tl_cuda_team_t *team = TASK_TEAM(task);
+    return ucc_tl_cuda_shm_barrier_test(UCC_TL_TEAM_RANK(team), task->bar);
+}
+
 static inline ucc_status_t ecopy(void *dst, void *src, size_t size,
-                                 ucc_ee_executor_t *      exec,
+                                 ucc_ee_executor_t       *exec,
                                  ucc_ee_executor_task_t **etask)
 {
     ucc_ee_executor_task_args_t exec_args = {0};
@@ -67,15 +93,47 @@ void ucc_tl_cuda_bcast_linear_progress(ucc_coll_task_t *coll_task)
     ucc_status_t        st;
     (void)team;
     (void)st;
-    ucc_ee_executor_t *     exec;
+    ucc_ee_executor_t      *exec;
     ucc_ee_executor_task_t *etask;
     ucc_status_t            status;
-    void *                  sbuf, *dbuf;
+    void                   *sbuf, *dbuf;
     task->super.status = UCC_INPROGRESS;
 
     status = ucc_coll_task_get_executor(&task->super, &exec);
     if (ucc_unlikely(status != UCC_OK)) {
         return;
+    }
+
+    switch (task->bcast_linear.stage) {
+    case STAGE_SYNC:
+        ucc_info("sync");
+        if (ucc_tl_cuda_get_sync(task) != UCC_OK) {
+            task->super.status = UCC_INPROGRESS;
+            return;
+        }
+        task->bcast_linear.step = 0;
+        ucc_info("setup");
+        st = ucc_tl_cuda_bcast_linear_setup_start(task);
+        if (st != UCC_OK) {
+            task->super.status = st;
+            return;
+        }
+        task->bcast_linear.stage = STAGE_SETUP;
+    case STAGE_SETUP:
+        ucc_info("test");
+        st = ucc_tl_cuda_bcast_linear_setup_test(task);
+        if (st != UCC_OK) {
+            task->super.status = st;
+            return;
+        }
+        ucc_tl_cuda_put_sync(task);
+        if (trank == 0 /* root */) {
+            task->bcast_linear.stage = STAGE_COPY;
+        } else {
+            task->bcast_linear.stage = STAGE_WAIT_ROOT;
+        }
+    default:
+        break;
     }
 
     if (trank == 0) {
@@ -141,7 +199,7 @@ void ucc_tl_cuda_bcast_linear_progress(ucc_coll_task_t *coll_task)
         case STAGE_CLIENT_COPY:
             dbuf   = task->bcast_linear.sbuf;
             sbuf   = TASK_SCRATCH(task,
-                                0); // need to copy from root's scratch buffer
+                                  0); // need to copy from root's scratch buffer
             status = ecopy(dbuf, sbuf, task->bcast_linear.size, exec,
                            &task->bcast_linear.exec_task);
             task->bcast_linear.stage = STAGE_CLIENT_COPY_WAIT;
@@ -155,7 +213,8 @@ void ucc_tl_cuda_bcast_linear_progress(ucc_coll_task_t *coll_task)
                     task->bcast_linear.exec_task = NULL;
                     ++task->bcast_linear.step;
                     set_rank_step(task, trank, task->bcast_linear.step, 0);
-                    task->bcast_linear.stage = STAGE_DONE; // TODO: just for debug
+                    task->bcast_linear.stage =
+                        STAGE_DONE; // TODO: just for debug
                 }
             }
             break;
@@ -172,8 +231,7 @@ ucc_status_t ucc_tl_cuda_bcast_linear_start(ucc_coll_task_t *coll_task)
 {
     ucc_tl_cuda_task_t *task  = ucc_derived_of(coll_task, ucc_tl_cuda_task_t);
     ucc_tl_cuda_team_t *team  = TASK_TEAM(task);
-    ucc_rank_t          trank = UCC_TL_TEAM_RANK(team);
-    ucc_coll_args_t *   args  = &TASK_ARGS(task);
+    ucc_coll_args_t    *args  = &TASK_ARGS(task);
     ucc_rank_t          tsize = UCC_TL_TEAM_SIZE(team);
     ucc_datatype_t      dt    = task->bcast_linear.dt;
 
@@ -181,12 +239,10 @@ ucc_status_t ucc_tl_cuda_bcast_linear_start(ucc_coll_task_t *coll_task)
     (void)args;
     (void)dt;
 
-    if (trank == 0) {
-        task->bcast_linear.stage = STAGE_COPY;
-    } else {
-        task->bcast_linear.stage = STAGE_WAIT_ROOT;
-    }
-    ucc_info("bcast start with dt: %ld and count: %ld", dt, args->src.info.count);
+    task->bcast_linear.stage = STAGE_SYNC;
+
+    ucc_info("bcast start with dt: %s and count: %ld", ucc_datatype_str(dt),
+             args->src.info.count);
 
     task->bcast_linear.size = ucc_dt_size(dt) * args->src.info.count;
 
@@ -199,8 +255,8 @@ ucc_status_t ucc_tl_cuda_bcast_linear_start(ucc_coll_task_t *coll_task)
 }
 
 ucc_status_t ucc_tl_cuda_bcast_linear_init(ucc_base_coll_args_t *coll_args,
-                                           ucc_base_team_t *     tl_team,
-                                           ucc_coll_task_t **    task_p)
+                                           ucc_base_team_t      *tl_team,
+                                           ucc_coll_task_t     **task_p)
 {
     ucc_tl_cuda_team_t *team = ucc_derived_of(tl_team, ucc_tl_cuda_team_t);
     ucc_tl_cuda_task_t *task;
@@ -219,13 +275,8 @@ ucc_status_t ucc_tl_cuda_bcast_linear_init(ucc_base_coll_args_t *coll_args,
         return status;
     }
 
-    // task->allgatherv_linear.get_count  = ucc_tl_cuda_allgather_get_count;
-    // task->allgatherv_linear.get_offset = ucc_tl_cuda_allgather_get_offset;
     task->bcast_linear.dt = coll_args->args.src.info.datatype;
-    ucc_info("bcast init with dt: %ld", task->bcast_linear.dt);
-
-    // task->allgatherv_linear.sbuf       = coll_args->args.src.info.buffer;
-    // task->allgatherv_linear.rbuf       = coll_args->args.dst.info.buffer;
+    ucc_info("bcast init with dt: %s", ucc_datatype_str(task->bcast_linear.dt));
 
     task->bcast_linear.sbuf = coll_args->args.src.info.buffer;
 
