e3cf8401ba682666a5c6e16d5fcd2ca70fbe8980
Yael Yacobovich <yyacobovich@nvidia.com>
2024-11-17 12:06:32 +0200

    fix ring

--- src/components/tl/ucp/allgather/allgather_ring.c
+++ src/components/tl/ucp/allgather/allgather_ring.c
@@ -94,7 +94,7 @@ ucc_status_t ucc_tl_ucp_allgather_ring_start(ucc_coll_task_t *coll_task)
     ucc_memory_type_t  rmem      = TASK_ARGS(task).dst.info.mem_type;
     ucc_datatype_t     dt        = TASK_ARGS(task).dst.info.datatype;
     ucc_rank_t         trank     = task->subset.myrank;
-    ucc_rank_t         rank      = UCC_TL_TEAM_RANK(team);
+    //ucc_rank_t         rank      = UCC_TL_TEAM_RANK(team);
     ucc_rank_t         tsize     = (ucc_rank_t)task->subset.map.ep_num;
     size_t             data_size = (count / tsize) * ucc_dt_size(dt);
     ucc_status_t       status;
@@ -104,7 +104,7 @@ ucc_status_t ucc_tl_ucp_allgather_ring_start(ucc_coll_task_t *coll_task)
     ucc_tl_ucp_task_reset(task, UCC_INPROGRESS);
 
     uint32_t USE_CUDA = UCC_TL_UCP_TEAM_LIB(team)->cfg.allgather_use_cuda;
-    task->count = 0;
+    
     if (!UCC_IS_INPLACE(TASK_ARGS(task))) {
         block = task->allgather_ring.get_send_block(&task->subset, trank, tsize, 0);
         if(USE_CUDA){
@@ -115,8 +115,9 @@ ucc_status_t ucc_tl_ucp_allgather_ring_start(ucc_coll_task_t *coll_task)
             }
         } else {
             /* Loopback */
-            UCPCHECK_GOTO(ucc_tl_ucp_send_nb(sbuf, data_size, smem, rank, team, task),task, out);
-            UCPCHECK_GOTO(ucc_tl_ucp_recv_nb(PTR_OFFSET(rbuf, data_size * block), data_size, rmem, rank, team, task),task, out);
+            ucc_rank_t tmp_rank = ucc_ep_map_eval(task->subset.map, trank);
+            UCPCHECK_GOTO(ucc_tl_ucp_send_nb(sbuf, data_size, smem, tmp_rank, team, task),task, out);
+            UCPCHECK_GOTO(ucc_tl_ucp_recv_nb(PTR_OFFSET(rbuf, data_size * block), data_size, rmem, tmp_rank, team, task),task, out);
         }
     }
     return ucc_progress_queue_enqueue(UCC_TL_CORE_CTX(team)->pq, &task->super);
