dba6666c0fe4d8a7d6ee40f2dac1ec8fe1990bf5
Ilya Kryukov <ilyakrukov@gmail.com>
2024-11-22 17:25:22 +0100

    TL/CUDA: remove unused atomic

--- src/utils/ucc_atomic.h
+++ src/utils/ucc_atomic.h
@@ -16,7 +16,6 @@
 #define ucc_atomic_add64          ucs_atomic_add64
 #define ucc_atomic_sub64          ucs_atomic_sub64
 #define ucc_atomic_cswap8         ucs_atomic_cswap8
-#define ucc_atomic_cswap32        ucs_atomic_cswap32
 #define ucc_atomic_cswap64        ucs_atomic_cswap64
 #define ucc_atomic_bool_cswap8    ucs_atomic_bool_cswap8
 #define ucc_atomic_bool_cswap64   ucs_atomic_bool_cswap64
