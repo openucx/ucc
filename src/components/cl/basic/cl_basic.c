79d06a979eaf3490875f3b52982cb2bd341eba18
ferrol aderholdt <faderholdt@nvidia.com>
2025-01-09 14:26:08 -0800

    CORE: Add support for ucc_mem_map/unmap
    CL/BASIC: add support for ucc_mem_map/unmap
    
    CL/HIER: add support for ucc_mem_map/unmap
    
    CL/DOCA_UROM: add support for ucc_mem_map/unmap
    
    TL/CUDA: add support for ucc_mem_map/unmap
    
    TL/MLX5: add support for ucc_mem_map/unmap
    
    TL/NCCL: add support for ucc_mem_map/unmap
    
    TL/RCCL: add support for ucc_mem_map/unmap
    
    TL/SELF: add support for ucc_mem_map/unmap
    
    TL/SHARP: add support for ucc_mem_map/unmap

--- src/components/cl/basic/cl_basic.c
+++ src/components/cl/basic/cl_basic.c
@@ -10,6 +10,13 @@ ucc_status_t ucc_cl_basic_get_lib_attr(const ucc_base_lib_t *lib,
                                        ucc_base_lib_attr_t  *base_attr);
 ucc_status_t ucc_cl_basic_get_context_attr(const ucc_base_context_t *context,
                                            ucc_base_ctx_attr_t      *base_attr);
+ucc_status_t ucc_cl_basic_mem_map(const ucc_base_context_t *context, int type,
+                                  void *address, size_t len, void *memh,
+                                  void *tl_h);
+ucc_status_t ucc_cl_basic_mem_unmap(const ucc_base_context_t *context, int type,
+                                    void *tl_h);
+ucc_status_t ucc_cl_basic_memh_pack(const ucc_base_context_t *context,
+                                    void *memh, void **packed_buffer);
 
 ucc_status_t ucc_cl_basic_get_lib_properties(ucc_base_lib_properties_t *prop);
 
