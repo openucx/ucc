39e49347965e3652224b7562de509ecca9f12804
ferrol aderholdt <faderholdt@nvidia.com>
2025-01-15 16:26:46 -0800

    CODESTYLE: fix code style

--- src/components/tl/self/tl_self_context.c
+++ src/components/tl/self/tl_self_context.c
@@ -50,20 +50,21 @@ ucc_tl_self_get_context_attr(const ucc_base_context_t *context, /* NOLINT */
     return UCC_OK;
 }
 
-ucc_status_t ucc_tl_self_mem_map(const ucc_base_context_t *context,
-                                int type, void *address,
-                                size_t len,
-                                void *memh, void *tl_h)
+ucc_status_t ucc_tl_self_mem_map(const ucc_base_context_t *context, int type,
+                                 void *address, size_t len, void *memh,
+                                 void *tl_h)
 {
     return UCC_ERR_NOT_SUPPORTED;
 }
 
-ucc_status_t ucc_tl_self_mem_unmap(const ucc_base_context_t *context, int type, void *memh)
+ucc_status_t ucc_tl_self_mem_unmap(const ucc_base_context_t *context, int type,
+                                   void *memh)
 {
     return UCC_ERR_NOT_SUPPORTED;
 }
 
-ucc_status_t ucc_tl_self_memh_pack(const ucc_base_context_t *context, void *memh, void **pack_buffer)
+ucc_status_t ucc_tl_self_memh_pack(const ucc_base_context_t *context,
+                                   void *memh, void **pack_buffer)
 {
     return UCC_ERR_NOT_SUPPORTED;
 }
