c5298113eb83bccb55118a8aba74f784a5484802
Sergey Lebedev <sergeyle@nvidia.com>
2025-03-12 19:01:29 +0100

    BUILD: Add support for optional filter argument in config parser print function (#1092)
    * BUILD: addd support for optional filter argument
    
    This commit adds compatibility for a newer version of UCX's config parser print function by conditionally adding an optional NULL filter argument. The changes include:
    
    - Modifying config/m4/ucx.m4 to check for the new function signature
    - Updating src/utils/ucc_parser.c and src/utils/ucc_parser.h to conditionally call the function with an additional NULL argument
    - Using preprocessor macros to maintain backward compatibility
    
    * REVIEW: fix review comments

--- src/utils/ucc_parser.c
+++ src/utils/ucc_parser.c
@@ -675,9 +675,8 @@ void ucc_config_parser_print_all_opts(FILE *stream, const char *prefix,
         }
 
         snprintf(title, sizeof(title), "%s configuration", entry->name);
-        ucs_config_parser_print_opts(stream, title, opts, entry->table,
+        UCS_CONFIG_PARSER_PRINT_OPTS(stream, title, opts, entry->table,
                                      entry->prefix, prefix, ucs_flags);
-
         ucs_config_parser_release_opts(opts, entry->table);
         ucc_free(opts);
     }
