534923dcdce1f321fd111e3c251e2ed0981bc3b7
Sergey Lebedev <sergeyle@nvidia.com>
2025-04-02 22:32:18 +0200

    TEST: build sm70 only in CI (#1106)

--- .ci/scripts/build_ucc.sh
+++ .ci/scripts/build_ucc.sh
@@ -9,7 +9,8 @@ mkdir -p "${UCC_SRC_DIR}/build"
 cd "${UCC_SRC_DIR}/build"
 "${UCC_SRC_DIR}/configure" --with-ucx="${UCX_INSTALL_DIR}" --with-cuda="${CUDA_HOME}" \
     --prefix="${UCC_INSTALL_DIR}" --enable-gtest --with-mpi \
-    --with-tls=cuda,nccl,self,sharp,shm,ucp,mlx5
+    --with-tls=cuda,nccl,self,sharp,shm,ucp,mlx5 \
+    --with-nvcc-gencode="-gencode=arch=compute_70,code=sm_70"
 make -j install
 echo "${UCC_INSTALL_DIR}/lib" > /etc/ld.so.conf.d/ucc.conf
 ldconfig
