baccaa14fcc88877038fdab748db967bcaeb365c
Andrii Bilokur <abilokur@nvidia.com>
2025-07-21 02:58:20 -0400

    CI: Migrate to CUDA 12.9, update GPU sm_86

--- .ci/scripts/build_ucc.sh
+++ .ci/scripts/build_ucc.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -eEx
 set -o pipefail
+export CFLAGS="-Wno-error=maybe-uninitialized"
+export CXXFLAGS="-Wno-error=maybe-uninitialized"
 
 echo "INFO: Build UCC"
 UCC_SRC_DIR="${SRC_DIR}/ucc"
@@ -10,7 +12,7 @@ cd "${UCC_SRC_DIR}/build"
 "${UCC_SRC_DIR}/configure" --with-ucx="${UCX_INSTALL_DIR}" --with-cuda="${CUDA_HOME}" \
     --prefix="${UCC_INSTALL_DIR}" --enable-gtest --with-mpi \
     --with-tls=cuda,nccl,self,sharp,shm,ucp,mlx5 \
-    --with-nvcc-gencode="-gencode=arch=compute_70,code=sm_70"
+    --with-nvcc-gencode="-gencode arch=compute_86,code=sm_86"
 make -j install
 echo "${UCC_INSTALL_DIR}/lib" > /etc/ld.so.conf.d/ucc.conf
 ldconfig
