b26c15ae15df21dda7169f843ad2e91012ea0524
Ilya Kryukov <ikryukov@nvidia.com>
2025-07-25 14:07:11 +0000

    TL/CUDA: missing iface

--- src/components/tl/cuda/tl_cuda.c
+++ src/components/tl/cuda/tl_cuda.c
@@ -9,6 +9,9 @@
 #include "components/mc/base/ucc_mc_base.h"
 #include "allgather/allgather.h"
 #include "allgatherv/allgatherv.h"
+#ifdef HAVE_TL_CUDA_NVLS
+#include "allreduce/allreduce.h"
+#endif
 #include "bcast/bcast.h"
 #include "reduce_scatter/reduce_scatter.h"
 #include "reduce_scatterv/reduce_scatterv.h"
@@ -115,6 +118,10 @@ __attribute__((constructor)) static void tl_cuda_iface_init(void)
         ucc_tl_cuda_allgather_algs;
     ucc_tl_cuda.super.alg_info[ucc_ilog2(UCC_COLL_TYPE_ALLGATHERV)] =
         ucc_tl_cuda_allgatherv_algs;
+#ifdef HAVE_TL_CUDA_NVLS
+    ucc_tl_cuda.super.alg_info[ucc_ilog2(UCC_COLL_TYPE_ALLREDUCE)] =
+        ucc_tl_cuda_allreduce_algs;
+#endif
     ucc_tl_cuda.super.alg_info[ucc_ilog2(UCC_COLL_TYPE_BCAST)] =
         ucc_tl_cuda_bcast_algs;
     ucc_tl_cuda.super.alg_info[ucc_ilog2(UCC_COLL_TYPE_REDUCE_SCATTER)] =
