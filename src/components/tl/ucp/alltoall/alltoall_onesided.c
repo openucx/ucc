72e3ffe40c5b6f66d5efae524b41b75d31f2c96c
ferrol aderholdt <faderholdt@nvidia.com>
2025-10-07 14:33:23 -0700

    REVIEW: address feedback

--- src/components/tl/ucp/alltoall/alltoall_onesided.c
+++ src/components/tl/ucp/alltoall/alltoall_onesided.c
@@ -198,13 +198,14 @@ ucc_status_t ucc_tl_ucp_alltoall_onesided_init(ucc_base_coll_args_t *coll_args,
     ucc_sbgp_t                  *sbgp;
 
     ALLTOALL_TASK_CHECK(coll_args->args, tl_team);
-    if (coll_args->args.mask & UCC_COLL_ARGS_FIELD_FLAGS) {
-        if (!(coll_args->args.flags & UCC_COLL_ARGS_FLAG_MEM_MAPPED_BUFFERS)) {
-            tl_error(UCC_TL_TEAM_LIB(tl_team),
-                     "non memory mapped buffers are not supported");
-            status = UCC_ERR_NOT_SUPPORTED;
-            return status;
-        }
+    if (!(coll_args->args.mask & UCC_COLL_ARGS_FIELD_FLAGS) ||
+        (coll_args->args.mask & UCC_COLL_ARGS_FIELD_FLAGS &&
+            (!(coll_args->args.flags &
+               UCC_COLL_ARGS_FLAG_MEM_MAPPED_BUFFERS)))) {
+        tl_error(UCC_TL_TEAM_LIB(tl_team),
+                 "non memory mapped buffers are not supported");
+        status = UCC_ERR_NOT_SUPPORTED;
+        return status;
     }
 
     if (!(coll_args->args.mask & UCC_COLL_ARGS_FIELD_MEM_MAP_SRC_MEMH)) {
