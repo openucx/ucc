98eee1deff0d6b076faed8a78f95b580b83ed131
ferrol aderholdt <faderholdt@nvidia.com>
2025-12-18 09:55:02 -0800

    TL/UCP: fix onesided a2a alg selection for 1ppn

--- src/components/tl/ucp/alltoall/alltoall_onesided.c
+++ src/components/tl/ucp/alltoall/alltoall_onesided.c
@@ -251,8 +251,10 @@ ucc_status_t ucc_tl_ucp_alltoall_onesided_init(ucc_base_coll_args_t *coll_args,
 
     sbgp = ucc_topo_get_sbgp(tl_team->topo, UCC_SBGP_NODE);
     if (sbgp->status == UCC_SBGP_NOT_EXISTS) {
-        status = UCC_ERR_NOT_SUPPORTED;
-        goto out;
+        /* 1 PPN, use put */
+        if (alg == UCC_TL_UCP_ALLTOALL_ONESIDED_AUTO) {
+            alg = UCC_TL_UCP_ALLTOALL_ONESIDED_PUT;
+        }
     }
     task                 = ucc_tl_ucp_init_task(coll_args, team);
     task->super.finalize = ucc_tl_ucp_alltoall_onesided_finalize;
